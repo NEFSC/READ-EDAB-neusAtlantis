737e3dffb77baa72fb1159d5d312f322ea500662
Gavin Fay <gfay42@gmail.com>
2016-09-20 18:13:29 -0400

    moving towards trunk v6066

--- at_test_call.sh
+++ at_test_call.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo Running Atlantis
-atlantisNew -i new_init.nc 0 -o atneus_v15_Test1_.nc -r at_run_neus_v15.prm -f at_force_neus_v15_DE_LINUX.prm -p at_physics_nuts_neus_v15_DE.prm -b at_biol_neus_v15_working.prm -h at_harvest_neus_v15_working.prm  -e at_economics_neus_DE_New.prm -s NeusGroups_v15_unix.csv -q NeusFisheries_v15.csv -d test3 1>1.junk 2>2.junk
+atlantisMerged -i new_init.nc 0 -o atneus_v15_Test1_.nc -r at_run_neus_v15.prm -f at_force_neus_v15_DE_LINUX.prm -p at_physics_nuts_neus_v15_DE.prm -b at_biol_neus_v15_working.prm -h at_harvest_neus_v15_working.prm  -e at_economics_neus_DE_New.prm -s NeusGroups_v15_unix.csv -q NeusFisheries_v15.csv -d 20160920 1>1.junk 2>2.junk
