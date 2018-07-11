09b19e8af132147cb5492e224369762118531360
RMORSEcode <morsere01@gmail.com>
2018-07-11 17:44:43 -0400

    fixed initial conditions file RN SN problems, changed length-weight relationships in biol file

--- R/merge_RN_SN.R
+++ R/merge_RN_SN.R
@@ -205,7 +205,8 @@ library(bgmfiles)
 wd2='C:/Users/ryan.morse/Documents/GitHub/atneus_RM'
 setwd(wd2)
 bgm.file <- ("neus_tmerc_RM.bgm")
-NEUS_15_init=make.sh.init.object(bgm.file, '20180710init2.nc') #'RMinit_newvalues2017.nc')
+# NEUS_15_init=make.sh.init.object(bgm.file, '20180710init2.nc') #'RMinit_newvalues2017.nc')
+NEUS_15_init=make.sh.init.object(bgm.file, 'RMinit_2018.nc') #'RMinit_newvalues2017.nc')
 sh.init(NEUS_15_init)
 newN=NEUS_15_init$df.nitrogen
 write.csv(t, file='benthic_species_N.csv', sep=',', col.names = T, row.names = F)
