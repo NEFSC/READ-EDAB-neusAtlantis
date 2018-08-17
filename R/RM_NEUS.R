f38d24d7867ec0d83379c2453d1761072a2f8265
RMORSEcode <morsere01@gmail.com>
2018-08-17 09:42:36 -0400

    update scaling for init vert biomass, add init nums

--- R/RM_NEUS.R
+++ R/RM_NEUS.R
@@ -400,6 +400,7 @@ NEUS_15_newestinit=make.sh.init.object(
   # 'C:/Users/ryan.morse/Documents/GitHub/atneus_RM/RMinit_newvalues2017.nc'
   'C:/Users/ryan.morse/Documents/GitHub/atneus_RM/RMinit_2018.nc'
 )
+NEUS_15_newestinit=make.sh.init.object( 'neus_tmerc_RM.bgm','RMinit_2018.nc')
 
 # 20180620
 bgm.file <- ("neus_tmerc_RM.bgm")
