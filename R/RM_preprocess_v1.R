05f2278635f60df685440ddd2221ce63476e0f6a
RMORSEcode <morsere01@gmail.com>
2019-03-03 16:15:19 -0500

    adding automation to R routine

--- R/RM_preprocess_v1.R
+++ R/RM_preprocess_v1.R
@@ -7,8 +7,8 @@ library("tidyr")
 # d1="C:/Users/ryan.morse/Documents/GitHub/atneus_RM_20180827"
 # d1='/home/ryan/Git/atneus737e3d' # for NEUS 1.0 on new code base RM
 
-d1='/home/ryan/Git/atneus_RM'
-d2='/home/ryan/AtlRuns/20190115a'
+# d1='/home/ryan/Git/atneus_RM'
+# d2='/home/ryan/AtlRuns/20190115a'
 
 d1='C:/Users/ryan.morse/Documents/GitHub/atneus_RM' #where (PRM, bgm, group data) are saved
 d2='E:/AtlantisRun/20161103/tes/20190228dta'
@@ -19,3 +19,7 @@ source(paste(d1,'/R/RM_preprocess_v2.R', sep=''))
 source(paste(d1,'/R/RM_atl_model_calibration.R', sep=''))
 
 save.image(paste(d2,"/ws.RData", sep=''))
+
+## use to call this script in bat file for windows:
+# cd C:\Users\ryan.morse\Documents\R\R-3.4.2.1\bin
+# Rscript -e "source(''C:/Users/ryan.morse/Documents/GitHub/atneus_RM/R/RM_preprocess_v1.R')"
