b5eeb3d8e64fd3c976ad38836aeb21c4b8b4a486
RMORSEcode <morsere01@gmail.com>
2019-03-22 17:20:45 -0400

    issue with SED

--- R/RM_preprocess_v1.R
+++ R/RM_preprocess_v1.R
@@ -9,11 +9,11 @@ library("tidyr")
 
 d1='/home/ryan/Git/atneus_RM'
 d2='/home/ryan/AtlRuns/20190318b'
-# d2='/media/ryan/Iomega_HDD/20190301dta'
+# # d2='/media/ryan/Iomega_HDD/20190301dta'
 
 
 d1='C:/Users/ryan.morse/Documents/GitHub/atneus_RM' #where (PRM, bgm, group data) are saved
-d2='E:/AtlantisRun/20161103/tes/20190315dta'
+d2='E:/AtlantisRun/20161103/tes/20190321dta'
 
 
 setwd(d2)
