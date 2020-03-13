67f8b2c9b4e75d07d1988c3cca2f10a292ec6acf
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-03-13 17:08:47 -0400

    minor reference fix

--- R/ROMS_Timeseries.R
+++ R/ROMS_Timeseries.R
@@ -17,39 +17,43 @@
 #' 
 #' #' Created by J. Caracappa
 
-year.dir = plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_OUT/1983/'
-which.face = 0:150
-which.box = 0:29
-plot.hflux = plot.statevar = plot.ltlvar = T
-which.levels = 4
+# year.dir = plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_OUT/1980/'
+# which.face = 0:150
+# which.box = 0:29
+# plot.hflux = plot.statevar = plot.ltlvar = T
+# which.levels = 4
 
 ROMS_Timeseries = function(year.dir, which.face = 0:150, which.box = 0:29, plot.hflux = T, plot.statevar = T, plot.ltlvar = T,which.levels = 4, plot.dir){
 
     nc.files = dir(year.dir,pattern = '*.nc')
     
-    bgm = rbgm::bgmfile(here::here('Geometry','neus_tmerc_RM2.bgm'))
+    # bgm = rbgm::bgmfile(here::here('Geometry','neus_tmerc_RM2.bgm'))
     
     if(plot.hflux){
-      
       hflux.file = nc.files[which(sapply(nc.files,function(x) any(strsplit(x,'[_.]+')[[1]]=='transport')))]
-      hflux.nc = ncdf4::nc_open(paste0(year.dir,hflux.file))
-      dest_b = ncdf4::ncvar_get(hflux.nc,'dest_boxid')
-      source_b = ncdf4::ncvar_get(hflux.nc,'source_boxid')
-      transport = ncdf4::ncvar_get(hflux.nc,'transport')
-      
-      for(f.id in which.face ){
-        plot.lab = paste0('Face',f.id,': From-Box',source_b[f.id])
-        DF = t(transport[1:which.levels,which(which.face == f.id),])
-        colnames(DF) = paste0('lev',1:which.levels)
-        
-        
-      }
-      
-      
-      
+      source(here::here('R','plot_roms_hflux.R'))
+      plot_roms_hflux(year.dir,hflux.file,which.face,which.levels,plot.dir)
     }
     
-  
-  
+    if(plot.statevar){
+      statevars.file = nc.files[which(sapply(nc.files,function(x) any(strsplit(x,'[_.]+')[[1]]=='statevars') & all(strsplit(x,'[_.]+')[[1]] != 'ltl')))]
+      source(here::here('R','plot_roms_statevars.R'))
+      plot_roms_statevars(year.dir,statevars.file,which.box,which.levels,plot.dir)
+    }
+    
+    if(plot.ltlvar){
+      ltlvars.file = nc.files[which(sapply(nc.files, function(x) any(strsplit(x,'[_.]+')[[1]] == 'ltl')))]
+      source(here::here('R','plot_roms_ltlvars.R'))
+      plot_roms_ltlvars(year.dir,ltlvars.file,which.box,which.levels,plot.dir)
+    }
   
 }
+
+ROMS_Timeseries(year.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_OUT/1980/',
+                plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_OUT/1980/',
+                which.face = 0:150,
+                which.box = 0:29,
+                which.levels = 4,
+                plot.hflux = T, 
+                plot.statevar = T,
+                plot.ltlvar = T)
