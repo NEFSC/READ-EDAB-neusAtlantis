6c02ee73cf09560890d0ccdd5afcdd5e87034345
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-03-20 12:18:02 -0400

    Adds option to upscael to biomass

--- R/plot_roms_ltlvars.R
+++ R/plot_roms_ltlvars.R
@@ -20,7 +20,10 @@
 # which.box = 0:29
 # which.levels = 4
 
-plot_roms_ltlvars = function(year.dir,ltlvars.file,which.box = 0:29,which.levels = 4, plot.dir){
+plot_roms_ltlvars = function(year.dir,ltlvars.file,which.box = 0:29,which.levels = 4, plot.dir,scale.volume=F,bgm.file,box.z.key){
+  
+
+  
   
   ltlvars.nc = ncdf4::nc_open(paste0(year.dir,ltlvars.file))
   boxes = ltlvars.nc$dim$boxes$vals
@@ -34,6 +37,33 @@ plot_roms_ltlvars = function(year.dir,ltlvars.file,which.box = 0:29,which.levels
   nbact = ncdf4::ncvar_get(ltlvars.nc,'nbact')
   neus.time = ncdf4::ncvar_get(ltlvars.nc,'time')
   real.time = as.POSIXct(neus.time,origin = '1964-01-01',tz = 'GMT')
+  
+  
+  if(scale.volume){
+    `%>%` = dplyr::`%>%`
+    bgm = rbgm::bgmfile(here::here('Geometry',bgm.file))
+    volume = dplyr::select(bgm$boxes,c(.bx0,area))
+    z.key = read.csv(here::here('Geometry',box.z.key))
+    box.volume = dplyr::left_join(volume,z.key,by='.bx0')
+    box.volume$volume = box.volume$area*box.volume$dz
+    box.volume$id = 1:nrow(box.volume)
+    for(lev in 1:dim(ndi)[1]){
+      box.volume2 = box.volume %>% dplyr::group_by(.bx0) %>% 
+        dplyr::summarize(rank = which.max(level),
+                         id = id[rank],
+                         volume = volume[rank],
+                         level = level[rank])
+      ndi[lev,box.volume2$.bx0+1,]=log(ndi[lev,box.volume2$.bx0+1,]*box.volume2$volume,10)
+      nlg[lev,box.volume2$.bx0+1,]=log(nlg[lev,box.volume2$.bx0+1,]*box.volume2$volume,10)
+      nlgz[lev,box.volume2$.bx0+1,]=log(nlgz[lev,box.volume2$.bx0+1,]*box.volume2$volume,10)
+      nmdz[lev,box.volume2$.bx0+1,]=log(nmdz[lev,box.volume2$.bx0+1,]*box.volume2$volume,10)
+      nsm[lev,box.volume2$.bx0+1,]=log(nsm[lev,box.volume2$.bx0+1,]*box.volume2$volume,10)
+      nsmz[lev,box.volume2$.bx0+1,]=log(nsmz[lev,box.volume2$.bx0+1,]*box.volume2$volume,10)
+      silg[lev,box.volume2$.bx0+1,]=log(silg[lev,box.volume2$.bx0+1,]*box.volume2$volume,10)
+      nbact[lev,box.volume2$.bx0+1,]=log(nbact[lev,box.volume2$.bx0+1,]*box.volume2$volume,10)
+      box.volume = box.volume[-which(box.volume$id %in% box.volume2$id),]
+    }
+  }
   
   ndi.plots = nlg.plots = nlgz.plots = nmdz.plots = nsm.plots = nsmz.plots = silg.plots = nbact.plots = list()
   
@@ -68,34 +98,41 @@ plot_roms_ltlvars = function(year.dir,ltlvars.file,which.box = 0:29,which.levels
     }
     dev.off()
   }
+
   
   for(b.id in seq_along(which.box) ){
     plot.lab = paste0('Box',which.box[b.id])
     plot.cols = RColorBrewer::brewer.pal(which.levels,'Set1')
     
     DF.ndi = reshape.var(ndi)
-    ndi.plots[[b.id]] = plot.var(DF.ndi,expression('Diazotroph Biomass (mgN'~m^-3*')'))
-    
     DF.nlg = reshape.var(nlg)
-    nlg.plots[[b.id]] = plot.var(DF.nlg,expression('Large Phytoplankton Biomass (mgN'~m^-3*')'))
-    
     DF.nlgz = reshape.var(nlgz)
-    nlgz.plots[[b.id]] = plot.var(DF.nlgz,expression('Large Zooplankton Biomass (mgN'~m^-3*')'))
-    
     DF.nmdz = reshape.var(nmdz)
-    nmdz.plots[[b.id]] = plot.var(DF.nmdz,expression('Medium Zooplankton Biomass (mgN'~m^-3*')'))
-    
     DF.nsm = reshape.var(nsm)
-    nsm.plots[[b.id]] = plot.var(DF.nsm,expression('Small Phytoplankton Biomass (mgN'~m^-3*')'))
-    
     DF.nsmz = reshape.var(nsmz)
-    nsmz.plots[[b.id]] = plot.var(DF.nsmz,expression('Small Zooplankton Biomass (mgN'~m^-3*')'))
-    
     DF.silg = reshape.var(silg)
-    silg.plots[[b.id]] = plot.var(DF.silg,expression('Large Phyoplankton Biomass (mgS'~m^-3*')'))
-
     DF.nbact = reshape.var(nbact)
-    nbact.plots[[b.id]] = plot.var(DF.nbact,expression('Bacterial Biomass (mgN'~m^-3*')'))
+    
+    if(scale.volume){
+      ndi.plots[[b.id]] = plot.var(DF.ndi,bquote('Diazotroph Biomass (mgN)'))
+      nlg.plots[[b.id]] = plot.var(DF.nlg,expression('Large Phytoplankton Biomass (mgN)'))
+      nlgz.plots[[b.id]] = plot.var(DF.nlgz,expression('Large Zooplankton Biomass (mgN)'))
+      nmdz.plots[[b.id]] = plot.var(DF.nmdz,expression('Medium Zooplankton Biomass (mgN)'))
+      nsm.plots[[b.id]] = plot.var(DF.nsm,expression('Small Phytoplankton Biomass (mgN)'))
+      nsmz.plots[[b.id]] = plot.var(DF.nsmz,expression('Small Zooplankton Biomass (mgN)'))
+      silg.plots[[b.id]] = plot.var(DF.silg,expression('Large Phyoplankton Biomass (mgS)'))
+      nbact.plots[[b.id]] = plot.var(DF.nbact,expression('Bacterial Biomass (mgN)'))
+    }else{
+      ndi.plots[[b.id]] = plot.var(DF.ndi,bquote('Diazotroph Biomass (mgN'~m^-3*')'))
+      nlg.plots[[b.id]] = plot.var(DF.nlg,expression('Large Phytoplankton Biomass (mgN'~m^-3*')'))
+      nlgz.plots[[b.id]] = plot.var(DF.nlgz,expression('Large Zooplankton Biomass (mgN'~m^-3*')'))
+      nmdz.plots[[b.id]] = plot.var(DF.nmdz,expression('Medium Zooplankton Biomass (mgN'~m^-3*')'))
+      nsm.plots[[b.id]] = plot.var(DF.nsm,expression('Small Phytoplankton Biomass (mgN'~m^-3*')'))
+      nsmz.plots[[b.id]] = plot.var(DF.nsmz,expression('Small Zooplankton Biomass (mgN'~m^-3*')'))
+      silg.plots[[b.id]] = plot.var(DF.silg,expression('Large Phyoplankton Biomass (mgS'~m^-3*')'))
+      nbact.plots[[b.id]] = plot.var(DF.nbact,expression('Bacterial Biomass (mgN'~m^-3*')'))
+    }
+
   
   }
   
