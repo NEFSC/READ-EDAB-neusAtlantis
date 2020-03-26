71b15357485298ec4573ef1e89f3be176781d7fc
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-03-26 10:37:06 -0400

    update output

--- R/ROMS maps allyears.R
+++ R/ROMS maps allyears.R
@@ -1,17 +1,17 @@
 source(here::here('R','ROMS_map_diagnostics.R'))
 
-roms_maps(roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing Files/',
-          plot.transport = F,
-          plot.statevars = T,
+roms_maps(roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_Files/',
+          plot.transport = T,
+          plot.statevars = F,
           plot.ltlvars = F,
           plot.yearly = T,
-          plot.seasonal = F,
-          plot.monthly = F,
+          plot.seasonal = T,
+          plot.monthly = T,
           plot.prefix = 'Allyears',
           plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Diagnostic_Figures/',
           bgm.file = 'neus_ll_WGS84.bgm')
 
-# roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing Files/'
+# roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_Files/'
 # plot.transport = T
 # plot.statevars = F
 # plot.ltlvars = F
