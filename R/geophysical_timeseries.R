3c70d9a9d9201685e7733704a9da3f4173b1b59a
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-04-22 11:35:46 -0400

    new example

--- R/geophysical_timeseries.R
+++ R/geophysical_timeseries.R
@@ -2,6 +2,7 @@
 
 source(here::here('R','plot_box_biophys_output.R'))
 source(here::here('R','plot_domain_biophys_output.R'))
+source(here::here('R','plot_geochem_comparisons.R'))
 
 nc.file = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Atlantis_Output_LTLForce_1980Fill/neus_output_test.nc'
 variable.names = c('NH3','NO3','Oxygen','Temp','salt','Light','Chl_a')
@@ -17,11 +18,24 @@ for(f in 1:length(variable.names)){
   #   plot.name = plot.names[f]
   # )
   
-  plot.domain.biophys(
-    nc.file = nc.file,
-    variable.name = variable.names[f],
+  # plot.domain.biophys(
+  #   nc.file = nc.file,
+  #   variable.name = variable.names[f],
+  #   plot.dir = plot.dir,
+  #   plot.name = plot.names[f]
+  # )
+  
+  plot.geochem.comps(
+    output.files = c(
+      'C:/Users/joseph.caracappa/Documents/Atlantis/Run_Files/atneus_v15_01272020/atneus_v15_01272020.nc',
+      'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Atlantis_Output_LTLForce_1980Fill/neus_output_test.nc',
+      'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Atlantis_Output_noLTLscale/neus_output_test.nc'
+    ),
+    model.names = c('original','LTL_Forced','LTL_noscale'),
+    var.name = variable.names[f],
     plot.dir = plot.dir,
-    plot.name = plot.names[f]
+    plot.name = paste0(variable.names[f],'_original_v_LTL_Forced')
   )
-
+  
+  print(f)
 }
