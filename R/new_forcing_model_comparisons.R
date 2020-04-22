53df376291cb975cb6fb2cf611613f04f90e2220
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-04-22 11:34:16 -0400

    add no LTL scaling comparison

--- R/new_forcing_model_comparisons.R
+++ R/new_forcing_model_comparisons.R
@@ -14,82 +14,96 @@ orig.model = 'C:/Users/joseph.caracappa/Documents/Atlantis/Run_Files/atneus_v15_
 new.physics = paste0(roms.dir,'Atlantis_Output/')
 new.ltl = paste0(roms.dir,'Atlantis_Output_LTLForce_1/')
 new.ltl.fill1980 = paste0(roms.dir,'Atlantis_Output_LTLForce_1980Fill/')
+ltl.noscale = paste0(roms.dir,'Atlantis_Output_noLTLscale/')
 figure.dir = paste0(roms.dir,'Diagnostic_Figures/Run_Comparisons/')
 
 #Original vs. new physics
-comp.model.groups(
-  model1.dir = orig.model,
-  model2.dir = new.physics,
-  model1.name = 'original model',
-  model2.name = 'new physics',
-  plot.raw = T,
-  plot.diff = T,
-  plot.out = paste(figure.dir,'orig_v_newphys_'),
-  table.out = T,
-  groups = NULL
-)
+# comp.model.groups(
+#   model1.dir = orig.model,
+#   model2.dir = new.physics,
+#   model1.name = 'original model',
+#   model2.name = 'new physics',
+#   plot.raw = T,
+#   plot.diff = T,
+#   plot.out = paste(figure.dir,'orig_v_newphys_'),
+#   table.out = T,
+#   groups = NULL
+# )
+# 
+# #Original vs. new ltl
+# comp.model.groups(
+#   model1.dir = orig.model,
+#   model2.dir = new.ltl,
+#   model1.name = 'original model',
+#   model2.name = 'new ltl',
+#   plot.raw = T,
+#   plot.diff = T,
+#   plot.out = paste(figure.dir,'orig_v_newltl_'),
+#   table.out = T,
+#   groups = NULL
+# )
+# 
+# #new physics vs. new LTL
+# comp.model.groups(
+#   model1.dir = new.physics,
+#   model2.dir = new.ltl,
+#   model1.name = 'new physics',
+#   model2.name = 'new ltl',
+#   plot.raw = T,
+#   plot.diff = T,
+#   plot.out = paste(figure.dir,'newphys_v_newltl_'),
+#   table.out = T,
+#   groups = NULL
+# )
+# 
+# #new LTL vs. new LTL 1980 fill
+# comp.model.groups(
+#   model1.dir = new.ltl,
+#   model2.dir = new.ltl.fill1980,
+#   model1.name = 'new ltl',
+#   model2.name = 'new ltl - filled 1980',
+#   plot.raw = T,
+#   plot.diff = T,
+#   plot.out = paste(figure.dir,'newltl_v_newltl1980_'),
+#   table.out = T,
+#   groups = NULL
+# )
+# 
+# #LTL 1980 fill vs original
+# comp.model.groups(
+#   model1.dir = new.ltl.fill1980,
+#   model2.dir = original.model,
+#   model1.name = 'new ltl - filled 1980',
+#   model2.name = 'original model',
+#   plot.raw = T,
+#   plot.diff = T,
+#   plot.out = paste(figure.dir,'newltl1980_v_orig_'),
+#   table.out = T,
+#   groups = NULL
+# )
+# 
+# #Ltl 1980 fill vs new physics
+# comp.model.groups(
+#   model1.dir = new.ltl.fill1980,
+#   model2.dir = new.physics,
+#   model1.name = 'new ltl - filled 1980',
+#   model2.name = 'new physics',
+#   plot.raw = T,
+#   plot.diff = T,
+#   plot.out = paste(figure.dir,'newltl1980_v_newphys_'),
+#   table.out = T,
+#   groups = NULL
+# )
 
-#Original vs. new ltl
-comp.model.groups(
-  model1.dir = orig.model,
-  model2.dir = new.ltl,
-  model1.name = 'original model',
-  model2.name = 'new ltl',
-  plot.raw = T,
-  plot.diff = T,
-  plot.out = paste(figure.dir,'orig_v_newltl_'),
-  table.out = T,
-  groups = NULL
-)
-
-#new physics vs. new LTL
-comp.model.groups(
-  model1.dir = new.physics,
-  model2.dir = new.ltl,
-  model1.name = 'new physics',
-  model2.name = 'new ltl',
-  plot.raw = T,
-  plot.diff = T,
-  plot.out = paste(figure.dir,'newphys_v_newltl_'),
-  table.out = T,
-  groups = NULL
-)
-
-#new LTL vs. new LTL 1980 fill
-comp.model.groups(
-  model1.dir = new.ltl,
-  model2.dir = new.ltl.fill1980,
-  model1.name = 'new ltl',
-  model2.name = 'new ltl - filled 1980',
-  plot.raw = T,
-  plot.diff = T,
-  plot.out = paste(figure.dir,'newltl_v_newltl1980_'),
-  table.out = T,
-  groups = NULL
-)
-
-#LTL 1980 fill vs original
+#Ltl no init scale vs new LTL fill 1980
 comp.model.groups(
   model1.dir = new.ltl.fill1980,
-  model2.dir = original.model,
+  model2.dir = ltl.noscale,
   model1.name = 'new ltl - filled 1980',
-  model2.name = 'original model',
+  model2.name = 'ltl no init scale',
   plot.raw = T,
   plot.diff = T,
-  plot.out = paste(figure.dir,'newltl1980_v_orig_'),
-  table.out = T,
-  groups = NULL
-)
-
-#Ltl 1980 fill vs new physics
-comp.model.groups(
-  model1.dir = new.ltl.fill1980,
-  model2.dir = new.physics,
-  model1.name = 'new ltl - filled 1980',
-  model2.name = 'new physics',
-  plot.raw = T,
-  plot.diff = T,
-  plot.out = paste(figure.dir,'newltl1980_v_newphys_'),
+  plot.out = paste(figure.dir,'newltl1980_v_noLTLscale_'),
   table.out = T,
   groups = NULL
 )
