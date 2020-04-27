d581b9dae3127e3fd3fdc9c3ff70b75a5c9f4826
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-04-27 10:28:54 -0400

    added nutrients

--- R/generate_forcing_gap_1964_1980.R
+++ R/generate_forcing_gap_1964_1980.R
@@ -3,7 +3,7 @@
 start.year = 1964
 
 # years = seq(start.year,1980)
-years = 1980
+years = 1964:1980
 
 source('C:/Users/joseph.caracappa/Documents/GitHub/neus-atlantis/R/make_spinup_forcing.R')
 
@@ -11,9 +11,12 @@ for(i in 1:length(years)){
   roms.out.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT output/'
   duplicate.forcing.year(
     roms.out.dir = roms.out.dir,
-    transport.file = paste0(roms.out.dir,'transport/roms_output_transport_tohydro_1981.nc'),
-    statevar.file = paste0(roms.out.dir,'statevars/roms_output_statevars_tohydro_1981.nc'),
+    # transport.file = paste0(roms.out.dir,'transport/roms_output_transport_tohydro_1981.nc'),
+    transport.file = NA,
+    # statevar.file = paste0(roms.out.dir,'statevars/roms_output_statevars_tohydro_1981.nc'),
+    statevar.file = NA,
     ltlvar.file = paste0(roms.out.dir,'ltl_statevars/roms_output_ltl_statevars_tohydro_1981.nc'),
+    nutvar.file = paste0(roms.out.dir,'nut_statevars/roms_output_nut_statevars_tohydro_1981.nc'),
     force.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_Files/',
     start.year = 1964,
     new.year = years[i],
