e183a87c584540b436e03fa458b8a5fb02e8fb88
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-05-22 09:04:22 -0400

    Updated call for generatlized summary function

--- R/call_ROMS_summary.R
+++ R/call_ROMS_summary.R
@@ -1,20 +1,28 @@
 dir = here::here()
 roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_OUT/'
-years = 1980:2014
+plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Diagnostic_Figures/Summary/'
+years = 1981:2014
+# years = 1981
 year.dirs = paste0(roms.dir,years,'/')
 
-source(here::here('R','ROMS_Timeseries.R'))
+source(here::here('R','plot_ROMS_summary.R'))
 
 
 for(i in 1:length(year.dirs)){
   
-  ROMS_Timeseries(year.dir = year.dirs[i],
+  plot_ROMS_summary(year.dir = year.dirs[i],
                   which.face = 0:150,
                   which.box = 0:29,
                   plot.hflux = T,
                   plot.statevar = T,
                   plot.ltlvar = T,
                   which.levels =4,
-                  plot.dir = year.dirs[i])
+                  plot.dir = plot.dir,
+                  plot.year = years[i],
+                  box.z.key = here::here('Geometry','box_depth_key.csv'),
+                  bgm.file = here::here('Geometry','neus_tmerc_RM2.bgm')
+  )
   print(years[i])
 }
+
+
