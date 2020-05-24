4046808b81016352d62471cb12ec1ae141b155dc
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-05-24 10:23:42 -0400

    Utility function to replace forcing variable names (need to match initial conditions file)

--- R/call_force_statevar_alternate.R
+++ R/call_force_statevar_alternate.R
@@ -8,10 +8,15 @@ roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT
 out.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_Files/Annual_Output/phys_statevars_alternate/'
 # roms.file = paste0(roms.dir,'roms_cobalt_v10_statevars_1981_neus_atl.nc')
 force.vars = c('temperature','salinity')
-var.units = c('deg C','psu')
+final.vars = c('Temp','salt')
+var.units = c('degrees Celcius','PSU')
+
+year.range = 1964:2014
 
 
 tempsalt.files = list.files(roms.dir,'*.nc',full.names = T)
+file.years = as.numeric(sort(gsub(".*_(\\d{4}).+","\\1",tempsalt.files)))
+tempsalt.files = tempsalt.files[which(file.years %in% year.range)]
 .packages = c("devtools","tidyverse","stringi","RNetCDF", "data.table")
 lapply(.packages, require, character.only=TRUE)
 source(here::here('R','make_force_statevar.R'))
@@ -22,6 +27,7 @@ for(i in 1:length(tempsalt.files)){
                       out.dir = out.dir,
                       force.vars = force.vars,
                       var.units = var.units,
+                      final.vars = final.vars,
                       out.prefix = 'roms_tempsalt_force_')
   print(i)
 }
@@ -30,15 +36,19 @@ roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT
 out.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_Files/Annual_Output/ltl_statevars/'
 # roms.file = paste0(roms.dir,'roms_cobalt_v10_statevars_1981_neus_atl.nc')
 force.vars = c('ndi','nlg','nlgz','nmdz','nsm','nsmz','silg','nbact')
+final.vars = c('ndi','Diatom_N','Carniv_Zoo_N','Zoo_N','PicoPhytopl_N','MicroZoo_N','Diatom_S','Pelag_Bact_N')
 var.units = c(rep('mg N m-3',6),'mg Si m-3','mg N m-3')
 
 ltl.files = list.files(roms.dir,'*.nc',full.names = T)
+file.years = as.numeric(sort(gsub(".*_(\\d{4}).+","\\1",ltl.files)))
+ltl.files = ltl.files[which(file.years %in% year.range)]
 for(i in 1:length(tempsalt.files)){
   make_force_statevar(roms.dir = roms.dir,
                       roms.file = ltl.files[i],
                       out.dir = out.dir,
                       force.vars = force.vars,
                       var.units = var.units,
+                      final.vars = final.vars,
                       out.prefix = 'roms_ltl_force_')
   print(i)
 }
@@ -47,15 +57,20 @@ roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT
 out.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_Files/Annual_Output/nut_statevars/'
 # roms.file = paste0(roms.dir,'roms_cobalt_v10_statevars_1981_neus_atl.nc')
 force.vars = c('nh4','no3','o2','sio4')
+final.vars = c('NH3','NO3','Oxygen','Si')
 var.units = c('mg N m-3','mg N m-3','mg O2 m-3','mg Si m-3')
 
 nut.files = list.files(roms.dir,'*.nc',full.names = T)
+file.years = as.numeric(sort(gsub(".*_(\\d{4}).+","\\1",nut.files)))
+nut.files = nut.files[which(file.years %in% year.range)]
+
 for(i in 1:length(nut.files)){
   make_force_statevar(roms.dir = roms.dir,
                       roms.file = nut.files[i],
                       out.dir = out.dir,
                       force.vars = force.vars,
                       var.units = var.units,
+                      final.vars = final.vars,
                       out.prefix = 'roms_nut_force_')
   print(i)
 }
