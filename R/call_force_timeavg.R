42816117c93cea3208ada24b5c4e7076adfcd387
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-05-24 10:24:52 -0400

    Add nutrients

--- R/call_force_timeavg.R
+++ R/call_force_timeavg.R
@@ -1,28 +1,29 @@
 source(here::here('R','plot_force_timeavg.R'))
 
-forcing.climatology(
-  force.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT output/statevars/',
-  file.pattern = 'salt_force_*',
-  plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Diagnostic_Figures/Forcing_Climatology/alt_',
-  time.group = 'ym',
-  plot.region = T
-  
-)
+file.patterns = c('roms_tempsalt_force_*','roms_ltl_force_*','roms_nut_force_*')
+folders = c('phys_statevars_alternate','ltl_statevars','nut_statevars')
 
-forcing.climatology(
-  force.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT output/statevars/',
-  file.pattern = 'temp_force_*',
-  plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Diagnostic_Figures/Forcing_Climatology/alt_',
-  time.group = 'ym',
-  plot.region = T
+for(i in 1:length(file.patterns)){
+  plot_force_timeavg(
+    force.dir = paste0('C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_Files/Annual_Output/',folders[i],'/'),
+    file.pattern = file.patterns[i],
+    plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Diagnostic_Figures/Forcing Aggregated Timeseries/',
+    time.group = 'ym',
+    plot.region = T
+  )
   
-)
+}
 
-forcing.climatology(
-  force.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT output/statevars/',
-  file.pattern = file.pattern = 'salt_force_*',
-  plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Diagnostic_Figures/Forcing_Climatology/alt_',
-  time.group = 'ym',
-  plot.region = T
+for(i in 1:length(file.patterns)){
+  plot_force_timeavg(
+    force.dir = paste0('C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_Files/Annual_Output/',folders[i],'/'),
+    file.pattern = file.patterns[i],
+    plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Diagnostic_Figures/Forcing Aggregated Timeseries/',
+    time.group = 'm',
+    plot.region = T
+  )
   
-)
+}
+
+
+
