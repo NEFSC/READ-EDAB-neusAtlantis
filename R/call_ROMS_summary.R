4315a34762e3612d6dac3fe77ed1a29f3fdc6f36
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-06-05 10:08:53 -0400

    Updated for new depth bins

--- R/call_ROMS_summary.R
+++ R/call_ROMS_summary.R
@@ -1,6 +1,6 @@
 dir = here::here()
-roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_OUT/'
-plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Diagnostic_Figures/Summary/'
+roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_OUT_new_levels/'
+plot.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Diagnostic_Figures/Summary_New_Levels/'
 years = 1981:2014
 # years = 1981
 year.dirs = paste0(roms.dir,years,'/')
@@ -8,19 +8,21 @@ year.dirs = paste0(roms.dir,years,'/')
 source(here::here('R','plot_ROMS_summary.R'))
 
 
-for(i in 1:length(year.dirs)){
+for(i in 2:length(year.dirs)){
   
   plot_ROMS_summary(year.dir = year.dirs[i],
                   which.face = 0:150,
-                  which.box = 0:29,
-                  plot.hflux = T,
+                  which.boxes = 0:29,
+                  plot.hflux = F,
                   plot.statevar = T,
-                  plot.ltlvar = T,
+                  plot.ltlvar = F,
+                  plot.nutvar = F,
                   which.levels =4,
                   plot.dir = plot.dir,
                   plot.year = years[i],
                   box.z.key = here::here('Geometry','box_depth_key.csv'),
-                  bgm.file = here::here('Geometry','neus_tmerc_RM2.bgm')
+                  bgm.file = here::here('Geometry','neus_tmerc_RM2.bgm'),
+                  scale.volume = F
   )
   print(years[i])
 }
