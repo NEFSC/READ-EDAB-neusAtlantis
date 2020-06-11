ab576de011baacbe637c890a28a3f5b31ccba41f
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-06-11 13:36:44 -0400

    updated paths and file calls

--- R/call_force_spinup.R
+++ R/call_force_spinup.R
@@ -8,17 +8,17 @@ years = 1964:1980
 source('C:/Users/joseph.caracappa/Documents/GitHub/neus-atlantis/R/make_force_spinup.R')
 
 for(i in 1:length(years)){
-  roms.out.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT output/'
+  roms.out.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT_Output/'
   make_force_spinup(
     roms.prefix = 'roms_cobalt_v10_',
     roms.out.dir = roms.out.dir,
-    # transport.file = paste0(roms.out.dir,'transport/roms_cobalt_v10_transport_1981_neus_atl.nc'),
-    transport.file = NA,
-    # statevar.file =paste0(roms.out.dir,'phys_statevars/roms_cobalt_v10_statevars_1981_neus_atl.nc'),
-    statevar.file = NA,
-    ltlvar.file = paste0(roms.out.dir,'ltl_statevars/roms_cobalt_v10_ltl_statevars_1981_neus_atl.nc'),
-    # nutvar.file = paste0(roms.out.dir,'nut_statevars/roms_cobalt_v10_nutvars_1981_neus_atl.nc'),
-    nutvar.file = NA,
+    transport.file = paste0(roms.out.dir,'transport/roms_cobalt_v10_transport_1981.nc'),
+    # transport.file = NA,
+    statevar.file =paste0(roms.out.dir,'phys_statevars/roms_cobalt_v10_statevars_1981.nc'),
+    # statevar.file = NA,
+    ltlvar.file = paste0(roms.out.dir,'ltl_statevars/roms_cobalt_v10_ltl_statevars_1981.nc'),
+    nutvar.file = paste0(roms.out.dir,'nut_statevars/roms_cobalt_v10_nutvars_1981.nc'),
+    # nutvar.file = NA,
     force.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_Files/',
     start.year = 1964,
     new.year = years[i],
