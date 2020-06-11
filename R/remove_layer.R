2d6722a4fca7f3f01849ba22893fd74a3127b634
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-06-11 13:41:41 -0400

    skeleton for script to crop ROMS layer (might be better in NCO)

---
+++ R/remove_layer.R
@@ -0,0 +1,8 @@
+#Script to remove and reformat forcing layers in Atlantis
+
+roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT_Output/transport/'
+force.file = paste0(roms.dir,'roms_cobalt_v10_transport_1981_neus_atl.nc')
+remove_layer(force.file,which.layer){
+  
+  
+}
