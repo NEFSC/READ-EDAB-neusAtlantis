4dbf624a1d82ba0416b58ae81968c344b673e82e
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-06-22 16:50:24 -0400

    Updated with proper variable names

--- R/call_force_statevar_alternate.R
+++ R/call_force_statevar_alternate.R
@@ -8,8 +8,13 @@ roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT
 out.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_Files/Annual_Output/phys_statevars_alternate/'
 # roms.file = paste0(roms.dir,'roms_cobalt_v10_statevars_1981.nc')
 force.vars = c('temperature','salinity')
-final.vars = c('Temp','salt')
+final.vars = c('temperature','salinity')
 var.units = c('degrees Celcius','PSU')
+long.names = c('Temperature','Salinity')
+fill.val = c(15,0)
+miss.val = c(15,0)
+valid.min = c(-2,0)
+valid.max = c(999,999)
 
 year.range = 1964:2014
 
@@ -28,6 +33,11 @@ for(i in 1:length(tempsalt.files)){
                       force.vars = force.vars,
                       var.units = var.units,
                       final.vars = final.vars,
+                      fill.val = fill.val,
+                      long.names = long.names,
+                      miss.val = miss.val,
+                      valid.min = valid.min,
+                      valid.max = valid.max,
                       out.prefix = 'roms_tempsalt_force_')
   print(i)
 }
@@ -35,9 +45,15 @@ for(i in 1:length(tempsalt.files)){
 roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/ROMS_COBALT_Output/ltl_statevars/'
 out.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_Files/Annual_Output/ltl_statevars/'
 # roms.file = paste0(roms.dir,'roms_cobalt_v10_statevars_1981.nc')
-force.vars = c('ndi','nlg','nlgz','nmdz','nsm','nsmz','silg','nbact')
-final.vars = c('ndi','Diatom_N','Carniv_Zoo_N','Zoo_N','PicoPhytopl_N','MicroZoo_N','Diatom_S','Pelag_Bact_N')
+# force.vars = c('ndi','nlg','nlgz','nmdz','nsm','nsmz','silg','nbact')
+final.vars = c('Diatom_N','Carniv_Zoo_N','Zoo_N','PicoPhytopl_N','MicroZoo_N','Diatom_S','Pelag_Bact_N')
+force.vars= final.vars
 var.units = c(rep('mg N m-3',6),'mg Si m-3','mg N m-3')
+long.names = c('Datiom Nitrogen','Large Zooplankton Nitrogen','Meso-Zooplankton Nitrogent','PicoPhytoplankton Nitrogen','Micro-Zooplankton Nitrogen','Diatom Silicon','Pelagic Bacteria Nitrogen')
+fill.val = c(0,0,0,0,0,0,0.2)
+miss.val = fill.val
+valid.min = rep(0,7)
+valid.max = rep(99999,7)
 
 ltl.files = list.files(roms.dir,'*.nc',full.names = T)
 file.years = as.numeric(sort(gsub(".*_(\\d{4}).+","\\1",ltl.files)))
@@ -49,6 +65,11 @@ for(i in 1:length(tempsalt.files)){
                       force.vars = force.vars,
                       var.units = var.units,
                       final.vars = final.vars,
+                      fill.val = fill.val,
+                      long.names = long.names,
+                      miss.val = miss.val,
+                      valid.min = valid.min,
+                      valid.max = valid.max,
                       out.prefix = 'roms_ltl_force_')
   print(i)
 }
@@ -59,6 +80,11 @@ out.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/Forcing_File
 force.vars = c('nh4','no3','o2','sio4')
 final.vars = c('NH3','NO3','Oxygen','Si')
 var.units = c('mg N m-3','mg N m-3','mg O2 m-3','mg Si m-3')
+fill.val = c(0,0,8000,0)
+miss.val = fill.val
+long.names = c('Ammonia','Nitrate','Dissovled Oxygen','Dissolved Silica')
+valid.min = rep(0,4)
+valid.max = rep(99999,4)
 
 nut.files = list.files(roms.dir,'*.nc',full.names = T)
 file.years = as.numeric(sort(gsub(".*_(\\d{4}).+","\\1",nut.files)))
@@ -71,6 +97,11 @@ for(i in 1:length(nut.files)){
                       force.vars = force.vars,
                       var.units = var.units,
                       final.vars = final.vars,
+                      fill.val = fill.val,
+                      long.names = long.names,
+                      miss.val = miss.val,
+                      valid.min = valid.min,
+                      valid.max = valid.max,
                       out.prefix = 'roms_nut_force_')
   print(i)
 }
