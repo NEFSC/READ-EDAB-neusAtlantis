9e79ed17800c92026acfb0e200d22556c6ed75a6
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-08-25 10:44:02 -0400

    add Hirata vs constant comparison

--- R/call_make_SatPhyto_forcing.R
+++ R/call_make_SatPhyto_forcing.R
@@ -3,40 +3,152 @@
 #Read in climatology function and statevar forcing function
 source(here::here('R','make_SatPhyto_files.R'))
 source(here::here('R','make_force_statevar.R'))
+source('C:/Users/joseph.caracappa/Documents/GitHub/neus-atlantis/R/make_force_spinup.R')
 
 #set.directories
 rawdata.dir = 'C:/Users/joseph.caracappa/Documents/Satellite_Phyto/Data/'
-# processed.dir = 'C:/Users/joseph.caracappa/Documents/Satellite_Phyto/Atlantis_Format/'
-force.dir = 'c:/Users/joseph.caracappa/Documents/Satellite_Phyto/Forcing_Files/'
+force.dir = 'c:/Users/joseph.caracappa/Documents/Satellite_Phyto/Forcing_Constant_PLDF/'
+force.dir.2 = 'c:/Users/joseph.caracappa/Documents/Satellite_Phyto/Forcing_Hirata_PLDF/'
 atl.varname =  c('Diatom_N','DinoFlag_N','PicoPhytopl_N','Diatom_S')
 atl.longname = c('Diatom Nitrogen','Dinoflagellate Nitrogen','PicoPhytoplankton Nitrogen','Diatom Silicate')
 
 #make climatology
 # raw.files = list.files(rawdata.dir,'D8-OCCCI-ATLANTIS_*')
 
-years = 1998:2017
+years = 1997:2019
 raw.files = paste0('D8-OCCCI-ATLANTIS_',years,'.csv')
 f=1
+
+#Forcing with Constant Diatom:Dinoflagellate ratio (2.7:1)
+phyto.fract.ls = list()
 for(f in 1:length(raw.files)){
   if(years[f] %% 4 == 0){
-    phyto.fract = matrix(0.75,nrow = 30, ncol = 366)
+    phyto.fract.ls[[f]] = matrix(2.7/3.7,nrow = 30, ncol = 366)
   }else{
-    phyto.fract = matrix(0.75,nrow = 30, ncol = 365)
+    phyto.fract.ls[[f]] = matrix(2.7/3.7,nrow = 30, ncol = 365)
   }
-
-  make_SatPhyto_files(in.dir = rawdata.dir,
-                      in.file = raw.files[f],
-                      out.dir = force.dir,
-                      out.file = paste0('Phyto_Forcing_',years[f]),
-                      stat.var = 'MED',
-                      bio.vars = c('MICRO','NANO','PICO'),
-                      atl.varname = atl.varname,
-                      atl.longname = atl.longname,
-                      atl.units = c(rep('mg N m-3',3),'mg Si m-3'),
-                      phyto.fract = phyto.fract,
-                      chl.conv = rep(7,3))
-  print(f)
 }
+
+#make the forcing files
+make_SatPhyto_files(in.dir = rawdata.dir,
+                    in.prefix = 'D8-OCCCI-ATLANTIS_*',
+                    out.dir = force.dir,
+                    out.prefix =  'Phyto_Forcing_',
+                    stat.var = 'MED',
+                    bio.vars = c('MICRO','NANO','PICO'),
+                    atl.varname = atl.varname,
+                    atl.longname = atl.longname,
+                    atl.units = c(rep('mg N m-3',3),'mg Si m-3'),
+                    phyto.fract.ls = phyto.fract.ls,
+                    chl.con = rep(7,3)
+                    )
+
+#Make Forcing using Hirata Diatom Proportion
+load('C:/Users/joseph.caracappa/Documents/Satellite_Phyto/Data/diatom_proportion_DOY.R')
+phyto.fract.ls = list()
+for(f in 1:length(raw.files)){
+  if(years[f] %% 4 == 0){
+    x = matrix(NA,nrow = 30, ncol = 366)
+    x[,1:365] = diatom.prop
+    x[,366] = x[,365]
+    phyto.fract.ls[[f]] = x
+  }else{
+    phyto.fract.ls[[f]] = diatom.prop
+  }
+}
+
+make_SatPhyto_files(in.dir = rawdata.dir,
+                    in.prefix = 'D8-OCCCI-ATLANTIS_*',
+                    out.dir = force.dir.2,
+                    out.prefix =  'Phyto_Forcing_Hirata_',
+                    stat.var = 'MED',
+                    bio.vars = c('MICRO','NANO','PICO'),
+                    atl.varname = atl.varname,
+                    atl.longname = atl.longname,
+                    atl.units = c(rep('mg N m-3',3),'mg Si m-3'),
+                    phyto.fract.ls = phyto.fract.ls,
+                    chl.con = rep(7,3)
+)
+
+#copy to obs hindcast directory
+obs.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Forcing_Files/Annual_Output/phyto_statevars_constant_PLDF/'
+obs.dir.2 = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Forcing_Files/Annual_Output/phyto_statevars_Hirata_PLDF/'
+from.files = paste0(force.dir,'Phyto_Forcing_',1998:2017,'.nc')
+from.files.2 = paste0(force.dir.2,'Phyto_Forcing_Hirata_',1998:2017,'.nc')
+file.copy(from.files,obs.dir,overwrite = T)
+file.copy(from.files.2,obs.dir.2,overwrite = T)
+
+#Make spinup files
+years = 1964:1997
+for(i in 1:length(years)){
+  # out.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Forcing_Files/Annual_Output/'
+  
+  make_force_spinup(
+    out.dir = obs.dir,
+    trans.prefix = NA,
+    statevar.prefix = NA,
+    phyto.prefix = 'Phyto_Forcing_',
+    transport.file = NA,
+    statevar.file = NA,
+    phyto.file = paste0(obs.dir,'Phyto_Forcing_1998.nc'),
+    # force.dir = paste0(out.dir,'Forcing_Files/'),
+    force.dir = obs.dir,
+    start.year = 1964,
+    new.year = years[i],
+    param.temp = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Forcing_Files/obs_hindcast_hydroconstruct_template.prm',
+    bat.temp = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Forcing_Files/hydroconstruct_run_template.bat'
+    )
+}
+
+#Copy files into GitHub directory
+from.files = paste0(obs.dir,'Phyto_Forcing_',1964:2017,'.nc')
+git.dir = 'C:/Users/joseph.caracappa/Documents/GitHub/neus-atlantis/currentVersion/tsfiles/Annual_Files/'
+file.copy(from.files,git.dir,overwrite = T)
+
+for(i in 1:length(years)){
+  # out.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Forcing_Files/Annual_Output/'
+  
+  make_force_spinup(
+    out.dir = obs.dir.2,
+    trans.prefix = NA,
+    statevar.prefix = NA,
+    phyto.prefix = 'Phyto_Forcing_Hirata_',
+    transport.file = NA,
+    statevar.file = NA,
+    phyto.file = paste0(obs.dir.2,'Phyto_Forcing_Hirata_1998.nc'),
+    # force.dir = paste0(out.dir,'Forcing_Files/'),
+    force.dir = obs.dir.2,
+    start.year = 1964,
+    new.year = years[i],
+    param.temp = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Forcing_Files/obs_hindcast_hydroconstruct_template.prm',
+    bat.temp = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Forcing_Files/hydroconstruct_run_template.bat'
+  )
+}
+
+#Copy files to Git Directory
+from.files = paste0(obs.dir.2,'Phyto_Forcing_Hirata_',1964:2017,'.nc')
+file.copy(from.files,git.dir,overwrite = T)
+
+# for(f in 1:length(raw.files)){
+#   if(years[f] %% 4 == 0){
+#     phyto.fract = matrix(0.75,nrow = 30, ncol = 366)
+#   }else{
+#     phyto.fract = matrix(0.75,nrow = 30, ncol = 365)
+#   }
+# 
+#   make_SatPhyto_files(in.dir = rawdata.dir,
+#                       in.file = raw.files[f],
+#                       out.dir = force.dir,
+#                       out.file = paste0('Phyto_Forcing_',years[f]),
+#                       stat.var = 'MED',
+#                       bio.vars = c('MICRO','NANO','PICO'),
+#                       atl.varname = atl.varname,
+#                       atl.longname = atl.longname,
+#                       atl.units = c(rep('mg N m-3',3),'mg Si m-3'),
+#                       phyto.fract = phyto.fract,
+#                       chl.conv = rep(7,3))
+#   print(f)
+# }
 
 #make forcing
 # satphyto.files = list.files(processed.dir,'Phyto_Atlantis',full.names = T)
--- R/call_run_comparisons.R
+++ R/call_run_comparisons.R
@@ -10,114 +10,67 @@
 source(here::here('R','plot_run_comparisons.R'))
 
 roms.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/ROMS_COBALT/'
-orig.model = 'C:/Users/joseph.caracappa/Documents/Atlantis/Run_Files/atneus_v15_01272020/'
-new.physics = paste0(roms.dir,'Atlantis_Output_2/')
-new.ltl = paste0(roms.dir,'Atlantis_Output_LTLForce_1/')
-new.ltl.fill1980 = paste0(roms.dir,'Atlantis_Output_LTLForce_1980Fill/')
-ltl.noscale = paste0(roms.dir,'Atlantis_Output_noLTLscale/')
-force.nutrients = paste0(roms.dir,'Atlantis_Output_NutrientForcing/')
-figure.dir = paste0(roms.dir,'Diagnostic_Figures/Run_Comparisons/')
+obs.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/'
+# orig.model = 'C:/Users/joseph.caracappa/Documents/Atlantis/Run_Files/atneus_v15_01272020/'
+# new.physics = paste0(roms.dir,'Atlantis_Output_2/')
+# new.ltl = paste0(roms.dir,'Atlantis_Output_LTLForce_1/')
+# new.ltl.fill1980 = paste0(roms.dir,'Atlantis_Output_LTLForce_1980Fill/')
+# ltl.noscale = paste0(roms.dir,'Atlantis_Output_noLTLscale/')
+# force.nutrients = paste0(roms.dir,'Atlantis_Output_NutrientForcing/')
+roms.cobalt = paste0(roms.dir,'Atlantis_Runs/Atlantis_Output_DinoFlag/')
+GLORYS = paste0(obs.dir,'Atlantis_Runs/GLORYS_Physics_1/')
+Satphyto = paste0(obs.dir,'Atlantis_Runs/SatPhyto_Forcing_1/')
+satphyto.const.diatom = paste0(obs.dir,'Atlantis_RUns/SatPhyto_Forcing_Const_Diatom_Prop/')
+satphyto.hirata.diatom = paste0(obs.dir,'Atlantis_RUns/SatPhyto_Forcing_Hirata_Diatom_Prop/')
+figure.dir = paste0(obs.dir,'Diagnostic_Figures/Run_Comparisons/')
 
-
-#Original vs. new physics
-# comp.model.groups(
-#   model1.dir = orig.model,
-#   model2.dir = new.physics,
-#   model1.name = 'original model',
-#   model2.name = 'new physics',
-#   plot.raw = T,
-#   plot.diff = T,
-#   plot.out = paste(figure.dir,'orig_v_newphys_'),
-#   table.out = T,
-#   groups = NULL
-# )
-# 
-# #Original vs. new ltl
-# comp.model.groups(
-#   model1.dir = orig.model,
-#   model2.dir = new.ltl,
-#   model1.name = 'original model',
-#   model2.name = 'new ltl',
-#   plot.raw = T,
-#   plot.diff = T,
-#   plot.out = paste(figure.dir,'orig_v_newltl_'),
-#   table.out = T,
-#   groups = NULL
-# )
-# 
-# #new physics vs. new LTL
-# comp.model.groups(
-#   model1.dir = new.physics,
-#   model2.dir = new.ltl,
-#   model1.name = 'new physics',
-#   model2.name = 'new ltl',
-#   plot.raw = T,
-#   plot.diff = T,
-#   plot.out = paste(figure.dir,'newphys_v_newltl_'),
-#   table.out = T,
-#   groups = NULL
-# )
-# 
-# #new LTL vs. new LTL 1980 fill
-# comp.model.groups(
-#   model1.dir = new.ltl,
-#   model2.dir = new.ltl.fill1980,
-#   model1.name = 'new ltl',
-#   model2.name = 'new ltl - filled 1980',
-#   plot.raw = T,
-#   plot.diff = T,
-#   plot.out = paste(figure.dir,'newltl_v_newltl1980_'),
-#   table.out = T,
-#   groups = NULL
-# )
-# 
-# #LTL 1980 fill vs original
-# comp.model.groups(
-#   model1.dir = new.ltl.fill1980,
-#   model2.dir = original.model,
-#   model1.name = 'new ltl - filled 1980',
-#   model2.name = 'original model',
-#   plot.raw = T,
-#   plot.diff = T,
-#   plot.out = paste(figure.dir,'newltl1980_v_orig_'),
-#   table.out = T,
-#   groups = NULL
-# )
-# 
-#Ltl 1980 fill vs new physics
-comp.model.groups(
-  model1.dir = new.ltl.fill1980,
-  model2.dir = new.physics,
-  model1.name = 'new ltl - filled 1980',
-  model2.name = 'new physics',
+#ROMS_COBALT vs GLORYS
+plot_run_comparisons(
+  model1.dir = roms.cobalt,
+  model2.dir = GLORYS,
+  model1.name = 'ROMS-COBALT',
+  model2.name = 'GLORYS',
   plot.raw = T,
   plot.diff = T,
-  plot.out = paste(figure.dir,'newltl1980_v_newphys_'),
+  plot.out = paste(figure.dir,'ROMS_v_GLORYS_'),
   table.out = T,
   groups = NULL
 )
 
-#Ltl no init scale vs new LTL fill 1980
-comp.model.groups(
-  model1.dir = new.ltl.fill1980,
-  model2.dir = ltl.noscale,
-  model1.name = 'new ltl - filled 1980',
-  model2.name = 'ltl no init scale',
+plot_run_comparisons(
+  model1.dir = roms.cobalt,
+  model2.dir = Satphyto,
+  model1.name = 'ROMS-COBALT',
+  model2.name = 'OCCCI + GLORYS',
   plot.raw = T,
   plot.diff = T,
-  plot.out = paste(figure.dir,'newltl1980_v_noLTLscale_'),
+  plot.out = paste(figure.dir,'ROMS_v_SatPhyto_'),
   table.out = T,
   groups = NULL
 )
 
-comp.model.groups(
-  model1.dir = ltl.noscale  ,
-  model2.dir = force.nutrients,
-  model1.name = 'Forced LTL',
-  model2.name = 'Forced Nutrients',
+plot_run_comparisons(
+  model1.dir = GLORYS,
+  model2.dir = Satphyto,
+  model1.name = 'GLORYS',
+  model2.name = 'OCCCI + GLORYS',
   plot.raw = T,
   plot.diff = T,
-  plot.out = paste(figure.dir,'newltl_v_newnutrients_'),
+  plot.out = paste(figure.dir,'GLORYS_v_SatPhyto'),
   table.out = T,
   groups = NULL
 )
+
+plot_run_comparisons(
+  model1.dir = satphyto.const.diatom,
+  model2.dir = satphyto.hirata.diatom,
+  model1.name = 'Sat. Const. ',
+  model2.name = 'Sat. Hirata',
+  plot.raw = T,
+  plot.diff = T,
+  plot.out = paste(figure.dir,'Const_v_Hirata'),
+  table.out = T,
+  groups = NULL
+)
+
+
