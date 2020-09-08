7b31b0fe20ec0e1d7fdc54741cfae1918de89379
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-09-08 15:19:25 -0400

    add print functionality

--- R/edit_init_param.R
+++ R/edit_init_param.R
@@ -15,35 +15,46 @@
 # overwrite = F
 # new.file.name =paste0(here::here('currentVersion'),'/neus_init_test.nc')
 
-edit_init_param = function(init.file,var.name, att.name,new.val,overwrite,new.file.name){
+edit_init_param = function(init.file,var.name,print.param,att.name,new.val,overwrite=F,new.file.name){
   
-  if(overwrite){
-    init.nc = ncdf4::nc_open(init.file,write = T)  
+  if(overwrite | print.param){
+    init.nc = ncdf4::nc_open(init.file,write = T)
+    if(print.param){
+      return(ncdf4::ncatt_get(init.nc,var.name,att.name))
+    }
   }else{
     file.copy(init.file,new.file.name)
     init.nc = ncdf4::nc_open(new.file.name,write = T)
   }
   
-  # ncdf4::ncatt_get(init.nc,var.name,att.name)
+  
+ 
   ncdf4::ncatt_put(init.nc,var.name,att.name,new.val)
   
   ncdf4::nc_close(init.nc)  
 }
 
 edit_init_param(
+  init.file = here::here('currentVersion','neus_init_FastSink.nc'),
+  var.name = 'Diatom_N',
+  att.name = 'svel',
+  print.param = T
+)
+
+edit_init_param(
   init.file = here::here('currentVersion','neus_init.nc'),
   var.name = 'Diatom_N',
   att.name = 'svel',
-  new.val = -4E-5,
-  overwrite = T,
-  new.file.name = NA
+  new.val = -4.6E-5,
+  overwrite = F,
+  new.file.name = here::here('currentVersion','neus_init_FastSink.nc')
 )
 
 edit_init_param(
   init.file = here::here('currentVersion','neus_init.nc'),
   var.name = 'Diatom_S',
   att.name = 'svel',
-  new.val = -4E-5,
-  overwrite = T,
-  new.file.name = NA
+  new.val = -4.6E-5,
+  overwrite = F,
+  new.file.name = here::here('currentVersion','neus_init_FastSink.nc')
 )
