edd144d0b86edf1837f324deb2c5a7e5b3f31799
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-09-21 16:54:24 -0400

    Fixed to work with older param file formats

--- R/edit_param_pprey.R
+++ R/edit_param_pprey.R
@@ -40,7 +40,7 @@ get_pprey_lines = function(atl.dir,biol.file){
   is.comment = sapply(biol.lines[pprey.lines],function(x) return(strsplit(x,'')[[1]][1] == '#'),USE.NAMES = F)
   #remove header lines
   pprey.names = biol.lines[pprey.lines[2]+1]
-  pprey.names = strsplit(pprey.names,'##| ')[[1]]
+  pprey.names = strsplit(pprey.names,'#|##| |\t')[[1]]
   pprey.names = pprey.names[which(nchar(pprey.names)>0)]
   
   pprey.lines = pprey.lines[!is.comment]
@@ -66,10 +66,10 @@ get_pprey_vals = function(atl.dir,biol.file,fgs.file,spp.names,is.pred,remove.ze
   name.id = seq(1,(length(pprey.lines)-1),2)
   val.id = seq(2,length(pprey.lines),2)
   
-  pred.names = sapply(pprey.lines[name.id],function(x) return(strsplit(x,'pPREY|pPREY1|pPREY2| ')[[1]][2]),USE.NAMES = F)
+  pred.names = sapply(pprey.lines[name.id],function(x) return(strsplit(x,'pPREY|pPREY1|pPREY2| |\t')[[1]][2]),USE.NAMES = F)
   pprey.mat = matrix(0,ncol = length(prey.names),nrow = length(pred.names))
   for(i in 1:length(pred.names)){
-    pprey.mat[i,]=as.numeric(strsplit(pprey.lines[val.id[i]]  ,' ')[[1]])
+    pprey.mat[i,]=as.numeric(strsplit(pprey.lines[val.id[i]]  ,' |\t')[[1]])
   }
   
   #If specifying predators, extract prey vals
