d90e693b1a825d847bdbd1ab7cb90ef9d3a5f1c5
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-10-19 12:36:33 -0400

    update example

--- R/plot_species_biomass.R
+++ R/plot_species_biomass.R
@@ -1,15 +1,17 @@
-atl.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Atlantis_Runs/Obs_Hindcast_RetunePlanktiv4/'
+atl.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Atlantis_Runs/RetunePlanktivores_NewMigration/'
 age.dat = read.table(paste0(atl.dir,'neus_outputAgeBiomIndx.txt'),header= T, stringsAsFactors = F)
 
+library(ggplot2)
 
-spp.name = 'HER'
+spp.name = 'GOO'
 
 new.dat = age.dat[,c(1,grep(spp.name,colnames(age.dat)))]
 colnames(new.dat)[-1] = sapply(colnames(new.dat)[-1],function(x) strsplit(x,paste0(spp.name,'.'))[[1]][2])
 new.dat2 = reshape2::melt(new.dat,id.var = 'Time',variable.name = 'Cohort',value.name = 'biomass')
 
-library(ggplot2)
+
 
 ggplot(new.dat2,aes(x= Time, y = biomass, col = Cohort))+
   geom_line(size = 1.2)+
-  xlim(0,2500)
+  xlim(10000,20000)+
+  ylim(0,5E5)
