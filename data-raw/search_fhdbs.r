d275cc6afeaa501284db5e35774eea87b2b45541
andybeet <22455149+andybeet@users.noreply.github.com>
2020-10-28 14:42:12 -0400

    added examples of how to run fhdbs functions

--- data-raw/search_fhdbs.r
+++ data-raw/search_fhdbs.r
@@ -10,16 +10,25 @@ download.file("https://github.com/NOAA-EDAB/ECSA/raw/master/data/allfhsg.RData",
 #'
 #' Uses the FHDBS (Food habits database) to identify prey item of a given species
 #'
+#' @param datafile dataframe. The foods habit data pull. (See `file` above)
 #' @param predator Numeric. SVSPP code to identify predator
 #' @param area Boolean. Break down prey by area predator was caught
 #' 
 #' @return tibble  nx 2
 #' \item{pynam}{Name of prey species}
 #' \item{garea}{Region predator was caught}
+#' 
+#' @examples 
+#' \dontrun{
+#' # pulls prey for Smooth dogfish (SVSPP = 13)
+#' find_prey(predator=13) 
+#' pulls prey for Smooth dogfish by area
+#' find_prey(predator=13,area =T)
+#' }
 #'
-find_prey <- function(file,predator,area=F) {
+find_prey <- function(datafile=file,predator,area=F) {
   # read in downloaded file
-  load(file) # called allfhsg
+  load(datafile) # called allfhsg
   
   prey <- allfhsg %>%
     dplyr::filter(svspp == predator) %>% 
@@ -41,6 +50,8 @@ find_prey <- function(file,predator,area=F) {
 
 #' Finds list of all predators in the food habits database
 #'
+#' @param datafile dataframe. The foods habit data pull. (See `file` above)
+#'
 #' @return tibble n x 7
 #' \item{SVSPP}{species code for survey data}
 #' \item{Code}{Atlantis functional group code}
@@ -49,9 +60,15 @@ find_prey <- function(file,predator,area=F) {
 #' \item{Scientific_Name}{Scientific name for species}
 #' \item{NESPP3}{species code for commercial fishing data}
 #' \item{Speciec_Itis}{Global species code}
+#' 
+#' #' @examples 
+#' \dontrun{
+#' # pulls all predators infor found in the FHDBS
+#' find_predators()
+#' }
 
-find_predators <- function(file) {
-  load(file) # load fh data
+find_predators <- function(datafile=file) {
+  load(datafile) # load fh data
   # read in functional group data
   fg <- readr::read_csv(here::here("data-raw","functionalGroupNames.csv"))
   preds <- allfhsg %>% 
