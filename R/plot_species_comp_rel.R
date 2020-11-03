748b21f56bfe56eb8457143e50ee4e6e168b91f6
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2020-11-02 20:00:54 -0500

    updated recent runs

--- R/plot_species_comp_rel.R
+++ R/plot_species_comp_rel.R
@@ -32,8 +32,8 @@ plot_species_comp_rel = function(atl.dir,spp.name, bio.log = F, bio.rel = T){
     theme_bw()
 }
 
-plot_species_comp_rel(atl.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Atlantis_Runs/ReducePred10/',
-                      spp.name = c('WPF', 'HER'),
-                      bio.log =F,
+plot_species_comp_rel(atl.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Atlantis_Runs/ReducePred13/',
+                      spp.name = c('WOL'),
+                      bio.log =T,
                       bio.rel = T)
   
