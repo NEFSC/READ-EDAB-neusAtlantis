822b9741e61fd696a17b23cee769c46bb32965c2
Robert Gamble <gambler_1650@yahoo.com>
2020-11-13 11:15:42 -0500

    Update catch_file_creator.R
    Added the timestep increase when a year is not present.  Previously it only occurred when a year was present.  The effect now is that missing years have 0 catch.

--- R/catch_file_creator.R
+++ R/catch_file_creator.R
@@ -1,20 +1,20 @@
 library(dplyr)
 library(data.table)
 library(stringr)
-# NOTE: THIS FILE NOT TO BE POSTED ON GITHUB DUE TO POTENTIAL CONFIDENTIALITY CONCERNS
+# NOTE: comland_meatwt_deflated.RData NOT TO BE POSTED ON GITHUB DUE TO POTENTIAL CONFIDENTIALITY CONCERNS
 # ask Sean for it
 
-# load("/home/rgamble/Desktop/Atlantis-Catch/comland_meatwt_deflated_stat_areas.RData")
-load('C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Diagnostic_Data/comland_meatwt_deflated_stat_areas.RData')
+load("C:/Users/robert.gamble/Desktop/Atlantis_Catch/comland_meatwt_deflated.RData")
+#load('C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Diagnostic_Data/comland_meatwt_deflated_stat_areas.RData')
 # from https://github.com/NOAA-EDAB/Atlantis-Catch-Files/blob/master/Atlantis_1_5_groups_svspp_nespp3.csv
 
-# spcodes <- readr::read_csv("/home/rgamble/atlantis/Atlantis_1_5_groups_svspp_nespp3.csv")
-spcodes = readr::read_csv(here::here('data-raw','Atlantis_1_5_groups_svspp_nespp3.csv'))
+spcodes <- readr::read_csv("C:/Users/robert.gamble/Desktop/Atlantis_Catch/Atlantis_1_5_groups_svspp_nespp3.csv")
+#spcodes = readr::read_csv(here::here('data-raw','Atlantis_1_5_groups_svspp_nespp3.csv'))
 spcodes <- filter(spcodes,!is.na(NESPP3))
 
 # Conversion factor to appropriate Atlantis units
-# CONVFACTOR = 40 / 71.902080
-CONVFACTOR = 0.278
+CONVFACTOR = 40 / 71.902080
+#CONVFACTOR = 0.278
 
 # time series in case we want to plot them
 
@@ -30,7 +30,7 @@ comlandts_atlantis_summary <- summarise(comlandts_atlantis_grouped, grpWGT = (su
 # Set up hindcast catch data
 hindcast_catch <- filter(comlandts_atlantis_summary,YEAR >= 1964)
 timesteps <- 54 * 365
-  
+
 # Set up catch tibble for time series file
 header <- c("MAK","HER","WHK","BLF","WPF","SUF","WIF","WTF","FOU","HAL","PLA","FLA","BFT","TUN","BIL","MPF","BUT","BPF","ANC","GOO","MEN","FDE","COD","SHK","OHK","POL","RHK","BSB","SCU","TYL","RED","OPT","SAL","DRM","STB","TAU","WOL","SDF","FDF","HAD","YTF","DOG","SMO","SSH","DSH","BLS","POR","PSH","WSK","LSK","SK","SB","PIN","REP","RWH","BWH","SWH","TWH","INV","LSQ","ISQ","SCA","QHG","CLA","BFF","BG","LOB","RCB","BMS","NSH","OSH","ZL","BD","MA","MB","SG","BC","ZG","PL","DF","MB","MMM","ZS","PB","BB","BO","DL","DR","DC")
 catch <- tibble(time = 0:timesteps)
@@ -46,6 +46,10 @@ for (sp in 2:91) {
       if (length(spp_ts$grpWGT[which(spp_ts$YEAR==year)] > 0)) {
         for (day in 1:365) {
           catch[timestep,sp] <- spp_ts$grpWGT[which(spp_ts$YEAR == year)]
+          timestep = timestep + 1
+        }
+      } else {
+        for (day in 1:365) {
           timestep = timestep + 1
         }
       }
