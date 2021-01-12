bf0ec47437433bc6982259f383cdcedfe9d99013
sgaichas <sgaichas@gmail.com>
2021-01-12 18:30:49 -0500

    move file pull to function, update persistence

--- diagnostics/DiagnosticConfig.R
+++ diagnostics/DiagnosticConfig.R
@@ -14,18 +14,6 @@ initial.conditions.file <- "neus_init.nc"
 fisheries.file <- "neus_fisheries.csv"
 biol.prm.file <- "at_biology.prm"
 
-# pull all files from google
-atlantisdrive::pull_from_drive(d.name,
-                               fileList = NULL, 
-                               googledriveFolder = g.name)
-
-# get scenario name from a file inside folder: SSB is unique
-scenario.name <- gsub("SSB.txt","",list.files(path=d.name, pattern = "*SSB.txt"))
-
-# output file names
-run.prm.file <- list.files(path=d.name, pattern = "*at_run*.xml")
-bioind.file <- paste0(scenario.name, "BiomIndx.txt")
-catch.file <- paste0(scenario.name, "Catch.txt")
 
 # define the hindcast period--these are survey years
 hindcast <- c(1980:2010)
