1bbb50b6e90b72df507c0f25824bec7126dd83e0
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2021-03-30 12:28:51 -0400

    FinalPersist_5 - Dropped other several fish mum/C - Diet fixes on BLF, WHK, SCA

--- R/plot_species_biomass.R
+++ R/plot_species_biomass.R
@@ -1,17 +1,18 @@
-atl.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Atlantis_Runs/ReducePred12/'
+atl.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Atlantis_Runs/FinalPersist_2/'
 age.dat = read.table(paste0(atl.dir,'neus_outputAgeBiomIndx.txt'),header= T, stringsAsFactors = F)
 
 library(ggplot2)
 
-spp.name = 'BLF'
+spp.name = 'DRM'
 
 new.dat = age.dat[,c(1,grep(spp.name,colnames(age.dat)))]
 colnames(new.dat)[-1] = sapply(colnames(new.dat)[-1],function(x) strsplit(x,paste0(spp.name,'.'))[[1]][2])
 new.dat2 = reshape2::melt(new.dat,id.var = 'Time',variable.name = 'Cohort',value.name = 'biomass')
 
-
-
+# new.dat2 = dplyr::filter(new.dat2, Cohort %in% c('1','2','3'))
 ggplot(new.dat2,aes(x= Time, y = biomass, col = Cohort))+
-  geom_line(size = 1.2)+
-  xlim(0,100)
+  geom_line(size = 1.2)
 
+xx = new.dat$`1`-new.dat$`0`
+plot(xx,type = 'l')
+abline(v = 5)
