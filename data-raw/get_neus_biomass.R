36bae5b84cca3a3d9272ff2d72787ec5cde924a0
andybeet <22455149+andybeet@users.noreply.github.com>
2021-04-17 17:27:50 -0400

    added box column for box specific biomass

--- data-raw/get_neus_biomass.R
+++ data-raw/get_neus_biomass.R
@@ -6,15 +6,32 @@
 #' Note: assumes catchability, q = 1 for all species, 
 #' and assumes a constant tow area over space and time
 
+library(magrittr)
+
 neus <- sf::st_read(here::here("Geometry","gis"),layer="Neus_ll_0p01",quiet=T)
 
+boxes <- neus %>% 
+  dplyr::filter(BOX_ID != c("23","24") ) %>%
+  dplyr::pull(BOX_ID)
+  
 
 channel <- dbutils::connect_to_database("server","user")
 survdatData <- survdat::get_survdat_data(channel)
-a <- survdat::calc_swept_area(survdatData$survdat,
+
+neusBiomass <- NULL
+
+for (box in boxes) {
+    
+  boxbio <- survdat::calc_swept_area(survdatData$survdat,
                          areaPolygon = neus,
+                         filterByArea = box,
                          areaDescription="BOX_ID",
                          filterBySeason = "all",
                          tidy=T)
+  boxbio$box <- box
+  neusBiomass <- rbind(neusBiomass,boxbio)
+  
+}
 
-saveRDS(a,file=here::here("data-raw","neusBiomass.RDS"))
+
+saveRDS(neusBiomass,file=here::here("data-raw","neusBiomass.RDS"))
