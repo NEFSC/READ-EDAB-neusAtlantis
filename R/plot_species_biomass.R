5f8d05483adbcc1d3b2ff05d1cf8abe56074e2a1
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2021-04-23 12:23:43 -0400

    option for cohort subset

--- R/plot_species_biomass.R
+++ R/plot_species_biomass.R
@@ -1,16 +1,16 @@
-atl.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Atlantis_Runs/FinalPersist_2/'
+atl.dir = 'C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Atlantis_Runs/BalanceHerStart/'
 age.dat = read.table(paste0(atl.dir,'neus_outputAgeBiomIndx.txt'),header= T, stringsAsFactors = F)
 
 library(ggplot2)
 
-spp.name = 'DRM'
+spp.name = 'HER'
 
 new.dat = age.dat[,c(1,grep(spp.name,colnames(age.dat)))]
 colnames(new.dat)[-1] = sapply(colnames(new.dat)[-1],function(x) strsplit(x,paste0(spp.name,'.'))[[1]][2])
 new.dat2 = reshape2::melt(new.dat,id.var = 'Time',variable.name = 'Cohort',value.name = 'biomass')
 
-# new.dat2 = dplyr::filter(new.dat2, Cohort %in% c('1','2','3'))
-ggplot(new.dat2,aes(x= Time, y = biomass, col = Cohort))+
+new.dat3 = dplyr::filter(new.dat2, Cohort %in% as.character(1:3))
+ggplot(new.dat3,aes(x= Time, y = biomass, col = Cohort))+
   geom_line(size = 1.2)
 
 xx = new.dat$`1`-new.dat$`0`
