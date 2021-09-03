5ebd7713044f40727888ef4c5d6cec8b7a6e8973
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2021-09-03 16:36:47 -0400

    Change for new post-processing

--- R/test post-processing.R
+++ R/test post-processing.R
@@ -1,5 +1,7 @@
 # Test post-processing procedure with new functions
-
+library(ncdf4)
+library(dplyr)
+library(atlantistools)
 ##Loads post-processing functions
 source(here::here('R','get_atl_paramfiles.R'))
 source(here::here('R','process_atl_output.R'))
@@ -14,11 +16,11 @@ run.name = 'ZM_Spatial_Final'
 
 atl.dir = paste0('C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Atlantis_Runs/',run.name,'/')
 
-dir.create(paste0(atl.dir,'Post_Processed/'))
+dir.create(paste0(atl.dir,'Post_Processed2/'))
+dir.create(paste0(atl.dir,'Post_Processed2/Data/'))
 param.dir = here::here ('currentVersion')
-out.dir = paste0(atl.dir,'Post_Processed/')
-
-
+out.dir = paste0(atl.dir,'Post_Processed2/Data/')
+fig.dir = paste0(atl.dir,'Post_Processed2/')
 
 #Run prefix is the filename prefix in the atlantis output (specified in run.bat)
 run.prefix = 'neus_output'
@@ -30,42 +32,31 @@ param.ls= get_atl_paramfiles(param.dir = param.dir,
                              include_catch=T)
 
 #Run  post-processing function to generate "result" R object. 
-
-process_atl_output( param.dir = here::here('currentVersion'),
+process_atl_output(
+  param.dir = here::here('currentVersion'),
   atl.dir = atl.dir,
   out.dir = out.dir,
-
   run.prefix = 'neus_output',
+  param.ls = param.ls,
   include_catch = T,
-  save.out = T, #If T, saves to file, if F returns to current environment
-  bgm.file = param.ls$bgm,
-  groups.file = param.ls$func.groups,
-  init.file = param.ls$init.nofill,
-  biol.prm = param.ls$biol.prm,
-  run.prm = param.ls$run.prm,
-  main.nc = param.ls$main.nc,
-  prod.nc = param.ls$prod.nc,
-  dietcheck = param.ls$dietcheck,
-  ssb = param.ls$ssb,
-  yoy = param.ls$yoy,
-  catch.file = param.ls$catch,
-  totcatch.file = param.ls$catchtot,
-  spatial.overlap = F #Warning takes a long time to run
-)
+  save.out = T,
+  diet.agg.time = 'month',
+  spatial.overlap = F,
+  large.file = F
+  )
 
 #If result object saved to file or already exists load it into env.
 # load(paste0(out.dir,'neus_output_postprocessed.rdata'))
-load(paste0(out.dir,'neus_output_postprocessed.Rdata'))
-library(ncdf4)
-library(dplyr)
+# load(paste0(out.dir,'neus_output_postprocessed.Rdata'))
+
 #Run diagnostic figures/tables script. See function document for more detailed description of figures.
 make_atlantis_diagnostic_figures(
   atl.dir = atl.dir,
+  fig.dir = fig.dir,
   out.dir = out.dir,
   param.dir = param.dir,
-  run.prefix = 'neus_output_test',
+  run.prefix = 'neus_output',
   run.name = run.name,
-  result = result,
   benthic.box =4,
   benthic.level = 4,
   
@@ -78,10 +69,11 @@ make_atlantis_diagnostic_figures(
   phytopl.history = here::here('R','phytoplankton_timeseries_biomass_tonnes_1998_2016.csv'),
   zoopl.history = here::here('R','Zooplankton_total_biomass_tonnes_N_20yrs.csv'),
  
+  plot.all = T,
   #Turn these on/off for desired output
   plot.benthic =F,
   plot.overall.biomass = F,
-  plot.biomass.timeseries = T,
+  plot.biomass.timeseries = F,
   plot.length.age = F,
   plot.biomass.box=F,
   plot.c.mum=F,
@@ -91,10 +83,10 @@ make_atlantis_diagnostic_figures(
   plot.physics=F,
   plot.growth.cons=F,
   plot.cohort=F,
-  plot.diet=T,
-  plot.consumption= T,
+  plot.diet=F,
+  plot.consumption= F,
   plot.spatial.biomass=F,
-  plot.LTL=F,
+  plot.LTL=T,
   plot.catch =T
 )
   
