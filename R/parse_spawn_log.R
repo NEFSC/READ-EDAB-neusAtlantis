03f160d292d020be9231ce2d6f31493604e001f6
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2022-01-04 11:20:27 -0500

    Edit to work with arbitrary number of log files

--- R/parse_spawn_log.R
+++ R/parse_spawn_log.R
@@ -1,18 +1,26 @@
 
 #Function to parse spawn debugging
-parse_spawn_log = function(file,spp = NULL){
+parse_spawn_log = function(run.dir,spp = NULL){
   
-  log.lines = readLines(file)  
-  spawn.lines  = grep('*TotSpawn*',log.lines)
+  log.files = list.files(run.dir,'^log*',full.names = T)
+  dat.df.ls = list()
   
-  spawn.dat =log.lines[spawn.lines]
-  
-  dat.df = read.table(text = gsub(':| ',',',spawn.dat),sep = ',')[,c(3,5,9,12,15,19,23,27,31,35,39,43,47,50,53)]
-  colnames(dat.df) = c('Time','Code','Cohort','Box','Layer','TotSpawn','IndSpawn','DEN','FSPB','SN','RN','FSP','KSPA','step1','RSprop')
-  
-  if(!is.null(spp)){
-    dat.df= subset(dat.df,Code %in% spp)
+  for(i in 1:length(log.files)){
+    
+    log.lines = readLines(log.files[i])  
+    spawn.lines  = grep('*TotSpawn*',log.lines)
+    
+    spawn.dat =log.lines[spawn.lines]
+    
+    dat.df = read.table(text = gsub(':| ',',',spawn.dat),sep = ',')[,c(3,5,9,12,15,19,23,27,31,35,39,43,47,50)]
+    colnames(dat.df) = c('Time','Code','Cohort','Box','Layer','TotSpawn','IndSpawn','DEN','FSPB','SN','RN','FSP','KSPA','step1')
+    
+    if(!is.null(spp)){
+      dat.df= subset(dat.df,Code %in% spp)
+    }
+    
+    dat.df.ls[[i]] = dat.df
   }
-  
-  return(dat.df)
+
+  return(dplyr::bind_rows(dat.df.ls))
 }
