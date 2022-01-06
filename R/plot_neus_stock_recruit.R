c17fd6067d1cc6fe1c56087c67263588638fcd23
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2022-01-06 13:02:07 -0500

    Integrated new make_recruit_calcs outputs

--- R/plot_neus_stock_recruit.R
+++ R/plot_neus_stock_recruit.R
@@ -1,6 +1,6 @@
 #Function to plot atlantis' stock-recruit relationship over output
 
-run.dir = "C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Atlantis_Runs/BH_NEUSv1_Spawn_Debug2/"
+run.dir = "C:/Users/joseph.caracappa/Documents/Atlantis/Obs_Hindcast/Atlantis_Runs/BH_NEUSv1_RescaleAlphaBeta_4/"
 
 
 plot_neus_stock_recruit = function(run.dir){
@@ -11,7 +11,7 @@ plot_neus_stock_recruit = function(run.dir){
   library(grid)
   
   #Functions
-  source(here::here('R','make_recruit_calcs.R'))
+  source(here::here('R','make_recruit_output.R'))
   source(here::here('R','get_recruit_type.R'))
   source(here::here('R','edit_param_BH.R'))
   source(here::here('R','parse_spawn_log.R'))
@@ -21,27 +21,35 @@ plot_neus_stock_recruit = function(run.dir){
     group_by(Time,Code,Box,Layer)%>%
     summarise(spawn = max(TotSpawn),DEN = sum(DEN))
   
-  spawn.tot = spawn.out %>%
-    group_by(Time,Code)%>%
-    summarise(Spawn = sum(spawn),
-              DEN = sum(DEN))%>%
-    mutate(date = as.POSIXct(Time*86400,origin = '1964-01-01 00:00:00',tz = 'UTC'),
-           year = format(date,format = '%Y'))%>%
-    ungroup()%>%
-    select(year,Code,Spawn,DEN)
+  # spawn.tot = spawn.out %>%
+  #   group_by(Time,Code)%>%
+  #   summarise(Spawn = sum(spawn),
+  #             DEN = sum(DEN))%>%
+  #   mutate(date = as.POSIXct(Time*86400,origin = '1964-01-01 00:00:00',tz = 'UTC'),
+  #          year = format(date,format = '%Y'))%>%
+  #   ungroup()%>%
+  #   select(year,Code,Spawn,DEN)
+  
+  spawn.tot = make_recruit_output(run.dir)%>%
+      mutate(date = as.POSIXct(time*365*86400,origin = '1964-01-01 00:00:00',tz = 'UTC'),
+             year = format(date,format = '%Y'))%>%
+    group_by(year,group)%>%
+    summarise(biomass = mean(biomass,na.rm=T))%>%
+      ungroup()%>%
+      rename(Code = 'group')%>%
+      select(year,Code,biomass)
   
   recruit.out = parse_recruit_log(run.dir) %>%
     # mutate(test = (BHalpha*Larvae)/(BHbeta+totfish))
-    group_by(Time,Code)%>%
-    summarise(recruit = mean(temprec),
-              biomass = mean(totfish),
-              spawn = mean(Larvae),
-              BHalpha = mean(BHalpha),
-              BHbeta = mean(BHbeta))%>%
-    mutate(date = as.POSIXct(Time*86400,origin = '1964-01-01 00:00:00',tz = 'UTC'),
+    group_by(time,code)%>%
+    summarise(recruit = mean(recruits),
+              spawn = mean(spawn)
+              )%>%
+    mutate(date = as.POSIXct(time*86400,origin = '1964-01-01 00:00:00',tz = 'UTC'),
            year = format(date,format = '%Y'))%>%
     ungroup()%>%
-    select(year,Code,recruit,biomass,spawn,BHalpha,BHbeta)
+    rename(Code = 'code')%>%
+    select(year,Code,recruit,spawn)
 
   
   # spawn = make_recruit_calc(run.dir)
@@ -69,11 +77,13 @@ plot_neus_stock_recruit = function(run.dir){
     
     dat.group = left_join(spawn.group,recruit.group)
    
-    alpha.group = mean(dat.group$BHalpha)
-    beta.group = mean(dat.group$BHbeta)
+    bh.group = bh %>% filter(group == group.names$group[i])
+    
+    alpha.group = bh.group$alpha
+    beta.group = bh.group$beta
     spawn.biomass = dat.group %>%
-      filter(Spawn != 0)%>%
-      mutate(spawn.biomass = Spawn/biomass)
+      filter(spawn != 0)%>%
+      mutate(spawn.biomass = spawn/biomass)
     spawn.biomass = mean(spawn.biomass$spawn.biomass,na.rm=T)
     biomass.group = mean(dat.group$biomass)
     
