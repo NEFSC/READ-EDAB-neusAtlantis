cc788e95a6d7072021a155d1b7d0df6eec8e4790
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2022-07-11 11:23:07 -0400

    Add option to skip groups not turned on

--- R/plot_biomass_catch.R
+++ R/plot_biomass_catch.R
@@ -2,7 +2,7 @@
 library(dplyr)
 library(ggplot2)
 
-run.dir = 'C:/Users/joe92/Documents/Atlantis/New_Init_Age_4/'
+run.dir = 'C:/Users/joe92/Documents/Atlantis/New_Init_Age_10/'
 
 biomass = read.table(paste0(run.dir,'neus_outputBiomindx.txt'),header = T)
   
@@ -15,32 +15,37 @@ plot.name = paste0(run.dir,'Post_Processed/Biomass_Catch.pdf')
 pdf(plot.name,onefile = T)
 for(i in 1:nrow(fgs)){
   
-  biomass.group = select(biomass,Time,all_of(fgs$Code[i])) %>%
-    mutate(variable = 'Biomass') %>%
-    rename(value = fgs$Code[i])  %>%
-    mutate(date = as.POSIXct(Time*86400,origin = '1964-01-01 00:00:00',tz = 'UTC'),
-           year = as.numeric(format(date,format = '%Y')))%>%
-    group_by(year,variable)%>%
-    summarise(value = mean(value,na.rm=T))%>%
-    ungroup()
+  if(fgs$IsTurnedOn[i] == 1){
+    biomass.group = select(biomass,Time,all_of(fgs$Code[i])) %>%
+      mutate(variable = 'Biomass') %>%
+      rename(value = fgs$Code[i])  %>%
+      mutate(date = as.POSIXct(Time*86400,origin = '1964-01-01 00:00:00',tz = 'UTC'),
+             year = as.numeric(format(date,format = '%Y')))%>%
+      group_by(year,variable)%>%
+      summarise(value = mean(value,na.rm=T))%>%
+      ungroup()
     
-  
-  catch.group = select(catch,Time,all_of(fgs$Code[i])) %>%
-    mutate(variable = 'Catch') %>%
-    rename(value = fgs$Code[i])%>%
-    mutate(date = as.POSIXct(Time*86400,origin = '1964-01-01 00:00:00',tz = 'UTC'),
-           year = as.numeric(format(date,format = '%Y')))%>%
-    group_by(year,variable)%>%
-    summarise(value = mean(value,na.rm=T))%>%
-    ungroup()
-  
-  data.all = bind_rows(biomass.group,catch.group)
- 
-  p = ggplot(data.all,aes(x= year, y = value, color = variable))+
-    geom_line()+
-    ggtitle(fgs$Code[i])
-  
-  gridExtra::grid.arrange(p)
+    
+    catch.group = select(catch,Time,all_of(fgs$Code[i])) %>%
+      mutate(variable = 'Catch') %>%
+      rename(value = fgs$Code[i])%>%
+      mutate(date = as.POSIXct(Time*86400,origin = '1964-01-01 00:00:00',tz = 'UTC'),
+             year = as.numeric(format(date,format = '%Y')))%>%
+      group_by(year,variable)%>%
+      summarise(value = mean(value,na.rm=T))%>%
+      ungroup()
+    
+    data.all = bind_rows(biomass.group,catch.group)
+    
+    p = ggplot(data.all,aes(x= year, y = value, color = variable))+
+      geom_line()+
+      ggtitle(fgs$Code[i])
+    
+    gridExtra::grid.arrange(p)
+  }else{
+    next()
+  }
+
 }
 
 dev.off()
