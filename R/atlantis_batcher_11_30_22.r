28ad4871c7e0f97dbaf1593fcbc67b7bfb9072b4
jcaracappa1 <57966543+jcaracappa1@users.noreply.github.com>
2023-01-12 21:34:59 -0500

    updated batcher

--- R/atlantis_batcher_11_30_22.r
+++ R/atlantis_batcher_11_30_22.r
@@ -16,18 +16,19 @@ atlantis_batcher = function(batcherFilename, userName, CHECK_TIME_INTERVAL = 30,
 
   notFinished <- TRUE
   
+  logFileSuffix <- strsplit(batcherFilename,".",fixed=TRUE)[[1]][1]
+  logFileSuffix_split <- strsplit(logFileSuffix,"/")[[1]]
+  logFileSuffix <- logFileSuffix_split[[length(logFileSuffix_split)]]
+  logfileName <- paste0("log_",logFileSuffix)
   columns_logfile <- c("Run_name", "Start_time", "End_time", "Status")
   logData <- data.frame(matrix(nrow = numRuns, ncol = length(columns_logfile)))
   colnames(logData) = columns_logfile
-  
+
   activeRunDirs <- folders[1:NUM_TO_RUN]
   numActiveRuns <- length(activeRunDirs)
   containerNameVector <- vector(mode="character")
 
-  n=1
   for (n in 1:NUM_TO_RUN) {
-    # param.dir <- paste0(here(),"/Rob_Project_Template/Project_Name_Version/")
-    # output.dir <- paste0(here(),"/Rob_Project_Template/Project_Name_Version/Atlantis_Runs")
     initFile <- batcherFile$InitNC[n]
     biolPrm <- batcherFile$BiolPrm[n]
     runPrm <- batcherFile$RunPrm[n]
@@ -51,8 +52,7 @@ atlantis_batcher = function(batcherFilename, userName, CHECK_TIME_INTERVAL = 30,
     print(param.dir)
     print(folders[1])
     outputFolder <- paste0('"',output.dir,folders[n],'"')
-    print(paste0(CONTAINER_TYPE," run -d --name ",  containerName, " --rm --mount \"type=bind,src=",param.dir,",dst=/app/model\" --mount \"type=bind,src=",outputFolder,"/,","dst=/app/model/output/\" atlantis_6536")
-    )
+    print(paste0(CONTAINER_TYPE," run -d --name ",  containerName, " --rm --mount \"type=bind,src=",param.dir,",dst=/app/model\" --mount \"type=bind,src=",outputFolder,"/,","dst=/app/model/output/\" atlantis_6536"))
     run <- paste0(CONTAINER_TYPE," run -d --name ",  containerName, " --rm --mount \"type=bind,src=",param.dir,",dst=/app/model\" --mount \"type=bind,src=",outputFolder,"/,","dst=/app/model/output/\" atlantis_6536")
   
     system(run)
@@ -60,8 +60,9 @@ atlantis_batcher = function(batcherFilename, userName, CHECK_TIME_INTERVAL = 30,
     logData$Start_time[n] <- format(Sys.time(), "%Y-%m-%d %H:%M:%S %Z")
     logData$End_time[n] < "Unfinished"
     logData$Status[n] <- "Not complete"
+    try(write.csv(logData,paste0(output.dir,'logFile.csv'), row.names = FALSE))
     
-    Sys.sleep(120)  
+    Sys.sleep(300)  
     file.copy(paste0(param.dir,'/RunAtlantis_base.sh'),paste0(param.dir,'/RunAtlantis.sh'),overwrite = T) 
   }
   
@@ -85,20 +86,20 @@ atlantis_batcher = function(batcherFilename, userName, CHECK_TIME_INTERVAL = 30,
           if (grepl(batcherFile$Run[bf_index], containerNameVector[i]) == TRUE) {
             logData$Status[bf_index] <- "Completed"
             logData$End_time[bf_index] <- format(Sys.time(), "%Y-%m-%d %H:%M:%S %Z")
+            try(write.csv(logData,paste0(output.dir,'logFile.csv'), row.names = FALSE))
           }
         }
         # If more runs, add in next run
         print(paste0("FINISHED: ", activeRunDirs[i]))
         activeRunDirs <- activeRunDirs[-i]
         containerNameVector <- containerNameVector[-i]
-        if (n < numRuns) {
+        numActiveRuns <- length(activeRunDirs)
+        if ((n < numRuns) && (numActiveRuns < NUM_TO_RUN)) {
           notFinished <- TRUE
           n <- n + 1
           print(paste0("n = ", n))
-          # param.dir <- paste0(here(),"/Rob_Project_Template/Project_Name_Version/")
-          # output.dir <- paste0(here(),"/Rob_Project_Template/Project_Name_Version/Atlantis_Runs/")
-          # param.dir = here::here('currentVersion','')
-          # output.dir = here::here('Atlantis_Runs','')
+          #param.dir = here::here('currentVersion','')
+          #output.dir = here::here('Atlantis_Runs','')
           initFile <- batcherFile$InitNC[n]
           biolPrm <- batcherFile$BiolPrm[n]
           runPrm <- batcherFile$RunPrm[n]
@@ -130,11 +131,14 @@ atlantis_batcher = function(batcherFilename, userName, CHECK_TIME_INTERVAL = 30,
           logData$End_time[n] < "Unfinished"
           logData$Status[n] <- "Not complete"
           
-          Sys.sleep(120)  
+          Sys.sleep(300)  
           activeRunDirs <- append(activeRunDirs, folders[n])  
           containerNameVector <- append(containerNameVector, containerName)
-        } else {
+          try(write.csv(logData,paste0(output.dir,'logFile.csv'), row.names = FALSE))
+        } else if ( n >= numRuns) {
           notFinished <- FALSE
+        } else {
+          notFinished <- TRUE
         }
       } else {
         notFinished <- TRUE
@@ -144,16 +148,5 @@ atlantis_batcher = function(batcherFilename, userName, CHECK_TIME_INTERVAL = 30,
     }  
     file.copy(paste0(param.dir,'/RunAtlantis_base.sh'),paste0(param.dir,'/RunAtlantis.sh'),overwrite = T)
   }
-#  notCompleted <- TRUE
-#  while (notCompleted) {
-#    Sys.sleep(CHECK_TIME_INTERVAL)
-#    container_ps_output <- system(paste0(CONTAINER_TYPE, " ps"), intern = TRUE)
-#    numContainers <- length(container_ps_output)
-#    if (numContainers == 0) {
-#      notCompleted <- FALSE
-#    }
-#  }
-#  write.csv(logData,"/net/work3/EDAB/atlantis/Rob_Project_Template/Project_Name_Version/Batcher_Test_1/logFile.csv", row.names = FALSE)
-  write.csv(logData,paste0(output.dir,'logFile.csv'), row.names = FALSE)
 }
 
