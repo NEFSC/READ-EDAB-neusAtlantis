aaf32e6956e72c4e219db05f4066d1ee067118e0
jcaracappa1 <joseph.caracappa@noaa.gov>
2023-03-03 11:46:49 -0500

    diagnostic checks of BH recruitment scenarios

--- R/BH_scenario_diagnostics.R
+++ R/BH_scenario_diagnostics.R
@@ -1,16 +1,48 @@
 library(dplyr)
+
+source(here::here('R','edit_param_BH.R'))
+
 batch.prefix = 'BH_convert_1'
 
 batch.dir = paste0('/home/jcaracappa/atlantis/Shared_Data/',batch.prefix)
 
 run.index = readRDS(here::here('data',paste0(batch.prefix,'_index.rds')))
 
+base.BH = readRDS(here::here('data','reference_BH_params.rds'))
+
 #Check if files are located in each run directory
 
-output.check = run.index
-output.check$has.output = NA
+output.check = run.index %>%
+  mutate(has.output = NA,
+         alpha.check = NA,
+         beta.check = NA,
+         maxT = NA)
 
-for(i in 1:length(dir.names)){ output.check$has.output[i] = length(list.files(paste0(batch.dir,'/',run.index$name[i])))>0}
+i=1
+for(i in 1:nrow(run.index)){
+  output.check$has.output[i] = length(list.files(paste0(batch.dir,'/',run.index$name[i])))>0
+  
+  bio.file =  here::here('currentVersion',paste0('at_biology_',run.index$name[i],'.prm'))
+  
+  ref.bh = base.BH %>% 
+    filter(Code == run.index$Code[i])
+  run.bh= get_param_BH(bio.prm = bio.file)%>%
+    filter(group == run.index$Code[i])
+  
+  real.alpha.scale = as.numeric(run.bh$alpha[1])/ref.bh$alpha[1]
+  real.beta.scale = as.numeric(run.bh$beta[1])/ref.bh$beta[1]
+    
+  output.check$alpha.check[i] = all.equal(real.alpha.scale,output.check$BHalpha[i])
+  output.check$beta.check[i] = all.equal(real.beta.scale, output.check$BHbeta[i])
+  
+  biom.file = paste0(batch.dir,'/',run.index$name[i],'/neus_outputBiomIndx.txt')
+  if(file.exists(biom.file) & file.size(biom.file)>0){
+    biom = read.table(biom.file,as.is = T,header = T)
+    output.check$maxT[i] = max(biom$Time,na.rm=T)  
+  }
+  
+  
+  }
 
 
 log.df = run.index %>% mutate(expected.file = NA,actual.file = NA, file.match = NA)
--- R/call_BH_scenarios.R
+++ R/call_BH_scenarios.R
@@ -7,9 +7,9 @@ source(paste0(repo.dir,'/R/atlantis_batcher.r'))
 atlantis_batcher(
   batcherFilename = paste0(repo.dir,'/Setup_Files/',paste0(batch.dir,'.csv')),
   userName            = 'jcara',
-  CHECK_TIME_INTERVAL = 600,
+  CHECK_TIME_INTERVAL = 60,
   NUM_TO_RUN          = 12,
   CONTAINER_TYPE      = 'podman',
-  param.dir = paste0(repo.dir,'/currentVersion/'),
+  param.dir = paste0(repo.dir,'currentVersion/'),
   output.dir = '/net/work3/EDAB/atlantis/Shared_Data/'
 )
