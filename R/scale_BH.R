c37f3bd269a45014b39085597a74bbcfdbe20940
jcaracappa1 <joseph.caracappa@noaa.gov>
2023-03-24 13:53:38 -0400

    Fix baseline BH to include all groups

--- R/scale_BH.R
+++ R/scale_BH.R
@@ -1,21 +1,25 @@
 #Script to scale Mum_C for age groups
+library(dplyr)
 source(here::here('R','edit_param_BH.R'))
 
 #Write all groups 
-base.BH = readRDS(here::here('data','reference_BH_params.rds'))
+# base.BH = readRDS(here::here('data','reference_BH_params.rds'))
+base.BH = get_param_BH(bio.prm = here::here('currentVersion','at_biology.prm'))%>%
+  mutate(alpha = as.numeric(alpha),beta = as.numeric(beta))
+
 # edit_param_BH(bio.prm = here::here('currentVersion','at_biology.prm'),
 #               group.name = base.BH$Code,
 #               alpha = base.BH$alpha,
 #               beta = base.BH$beta,
 #               overwrite = T)
 
-group.name = 'WPF'
+group.name = 'GOO'
 
 alpha.scale = 2
 beta.scale =0.5
 
-new.alpha = base.BH$alpha[which(base.BH$Code == group.name)]*alpha.scale
-new.beta = base.BH$beta[which(base.BH$Code == group.name)]*beta.scale
+new.alpha = base.BH$alpha[which(base.BH$group == group.name)]*alpha.scale
+new.beta = base.BH$beta[which(base.BH$group == group.name)]*beta.scale
 
 edit_param_BH(bio.prm = here::here('currentVersion','at_biology.prm'),
               group.name = group.name,
