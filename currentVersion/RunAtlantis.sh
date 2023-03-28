537ac582eb1cb3c771a242a840ea77f83da94eea
jcaracappa1 <joseph.caracappa@noaa.gov>
2023-03-28 14:40:35 -0400

    removed junk files due to podman error

--- currentVersion/RunAtlantis.sh
+++ currentVersion/RunAtlantis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 find /app/model -type f | xargs dos2unix
-atlantisMerged -i neus_init.nc 0 -o neus_output.nc -r at_run.prm -f at_force_LINUX.prm -p at_physics.prm -b at_biology.prm -h at_harvest.prm -e at_economics.prm -s neus_groups.csv -q neus_fisheries.csv -t . -d output 1\>1a.junk 2\>2a.junk
+atlantisMerged -i neus_init.nc 0 -o neus_output.nc -r at_run.prm -f at_force_LINUX.prm -p at_physics.prm -b at_biology.prm -h at_harvest.prm -e at_economics.prm -s neus_groups.csv -q neus_fisheries.csv -t . -d output
