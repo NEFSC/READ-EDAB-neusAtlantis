4c9fb04d09048b7d66c8a73a9e6604127f13aaf3
Joseph Caracappa (NOAA) <57966543+jcaracappa1@users.noreply.github.com>
2023-03-30 14:59:24 -0400

    test cluster R script

---
+++ slurm_test.R
@@ -0,0 +1,1 @@
+sudo podman run --rm  -d --name scenario1 --mount "type=bind,src=/usr/local/neus-atlantis/currentVersion/,dst=/app/model" --mount "type=bind,src=/contrib/Joseph.Caracappa/out1,dst=/app/model/output/" atlantis:6536
