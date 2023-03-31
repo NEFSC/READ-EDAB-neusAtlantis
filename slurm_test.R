135ab66a55bb2da4442b45cf850f82ed5547b951
Joseph Caracappa (NOAA) <57966543+jcaracappa1@users.noreply.github.com>
2023-03-30 20:11:51 -0400

    Update slurm_test.R

--- slurm_test.R
+++ slurm_test.R
@@ -1,2 +1,3 @@
-mkdir /contrib/Joseph.Caracappa/out1
-sudo podman run --rm  -d --name scenario1 --mount "type=bind,src=/usr/local/neus-atlantis/currentVersion/,dst=/app/model" --mount "type=bind,src=/contrib/Joseph.Caracappa/out1,dst=/app/model/output/" atlantis:6536
+dir.create("/contrib/Joseph.Caracappa/out1/")
+x= 'sudo podman run --rm  -d --name scenario1 --mount "type=bind,src=/usr/local/neus-atlantis/currentVersion/,dst=/app/model" --mount "type=bind,src=/contrib/Joseph.Caracappa/out1,dst=/app/model/output/" atlantis:6536'
+system(x)
