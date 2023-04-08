851b2ea5f9462c2aad51208a319a49545f9a7350
jcaracappa1 <joseph.caracappa@noaa.gov>
2023-04-08 12:19:34 -0400

    verbose off

--- R/call_run_comparisons.R
+++ R/call_run_comparisons.R
@@ -2,16 +2,19 @@
 source(here::here('R','plot_run_comparisons.R'))
 source(here::here('R','plot_run_catch_comparisons.R'))
 
-dev = '/home/jcaracappa/atlantis/Shared_Data/Dev_Runs/Dev_20230324/'
+dev = '/home/jcaracappa/atlantis/Shared_Data/Dev_Runs/Dev_20230327/'
+v6665 = here::here('Atlantis_Runs','v6665_test','')
+v6665.nm = here::here('Atlantis_Runs','v6665_test_nomig','')
+v6665.cal = here::here('Atlantis_Runs','v6665_calibrated','')
 
 figure.dir = here::here('Figures','Run_Comparisons','')
 
 plot_run_comparisons(
-  model.dirs = c(dev,bh.g,bh.h),
-  model.names = c('Dev_20230324','BH_convert_2_final_g','BH_convert_2_final_h'),
-  plot.rel = T,
+  model.dirs = c(dev,v6665,v6665.cal),
+  model.names = c('Dev_20230327','v6665_test','v6665_calibrated'),
+  plot.rel = F,
   plot.diff = F,
-  plot.out = paste(figure.dir,'BH_convert_2_final_h_'),
+  plot.out = paste(figure.dir,'v6536_v_v6665_calibrated_'),
   table.out = F,
   groups = NULL,
   remove.init = F
