79bacd8564aaaac480a4aa0b95a287981dcf92c6
jcaracappa1 <joseph.caracappa@noaa.gov>
2023-05-03 11:49:43 -0400

    Reorganized directories

--- R/Post_Processing/call_run_comparisons.R
+++ R/Post_Processing/call_run_comparisons.R
@@ -2,8 +2,8 @@
 source(here::here('R','plot_run_comparisons.R'))
 source(here::here('R','plot_run_catch_comparisons.R'))
 
-dev.old = '/home/jcaracappa/atlantis/Shared_Data/Dev_Runs/Dev_20230313/'
-dev = '/home/jcaracappa/atlantis/Shared_Data/Dev_Runs/Dev_20230327/'
+dev.old = '/net/work3/EDAB/atlantis/Shared_Data/Dev_Runs/Dev_20230313/'
+dev = '/net/work3/EDAB/atlantis/Shared_Data/Dev_Runs/Dev_20230327/'
 zl.1 = here::here('Atlantis_Runs','ZL_restore_1','')
 zl.2 = here::here('Atlantis_Runs','ZL_restore_2','')
 zl.2b = here::here('Atlantis_Runs','ZL_restore_2b','')
@@ -34,11 +34,11 @@ plot_run_comparisons(
 
 
 plot_run_comparisons(
-  model.dirs = c(dev.old,dev,zl.7),
-  model.names = c('Dev_20230313','Dev_20230327','ZL_restore_7'),
+  model.dirs = c(dev,mak2),
+  model.names = c('Dev_20230327','MAK_fix_2'),
   plot.rel = T,
   plot.diff = F,
-  plot.out = paste(figure.dir,'ZL_restore_7_v_dev'),
+  plot.out = paste(figure.dir,'PR204_v_dev_'),
   table.out = F,
   groups = NULL,
   remove.init = F
