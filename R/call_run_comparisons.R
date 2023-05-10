0e6970f611ab20c57e3b843cacdffd6dc2ebdeb7
jcaracappa1 <joseph.caracappa@noaa.gov>
2023-05-10 18:33:18 -0400

    revert merge mistakes

--- R/call_run_comparisons.R
+++ R/call_run_comparisons.R
@@ -5,20 +5,19 @@ source(here::here('R','plot_run_catch_comparisons.R'))
 dev.old = '/net/work3/EDAB/atlantis/Shared_Data/Dev_Runs/Dev_20230313/'
 # dev = '/net/work3/EDAB/atlantis/Rob_proj/Atlantis_Runs/dev_4_14_23/'
 dev = '/net/work3/EDAB/atlantis/Rob_proj/Atlantis_Runs/MAK_pers_37/'
-ZL.revert = here::here('Atlantis_Runs','ZL_revert_1','')
-ZL.revert2 = here::here('Atlantis_Runs','ZL_revert_2','')
-ZL.revert2b = here::here('Atlantis_Runs','ZL_revert_2b','')
-recruit.12 = here::here('Atlantis_Runs','all_recruit_12','')
-ZL.revert3 = here::here('Atlantis_Runs','ZL_revert_3','')
+v6665.4 = here::here('Atlantis_Runs','v6665_MiscCalib_4','')
+v6665.5 = here::here('Atlantis_Runs','v6665_MiscCalib_5','')
+v6665.5b = here::here('Atlantis_Runs','v6665_MiscCalib_5b','')
+v6665.6 = here::here('Atlantis_Runs','v6665_MiscCalib_6','')
 
 figure.dir = here::here('Figures','Run_Comparisons','')
 
 plot_run_comparisons(
-  model.dirs = c(dev.old,dev,ZL.revert,recruit.12),
-  model.names = c('dev_old','dev','ZL_revert_1','all_recruit_12'),
+  model.dirs = c(dev,v6665.4,v6665.6),
+  model.names = c('dev','v6665_MiscCalib_4','v6665_MiscCalib_6'),
   plot.rel = T,
   plot.diff = F,
-  plot.out = paste(figure.dir,'all_recruit_12'),
+  plot.out = paste(figure.dir,'v6665_MiscCalib_6'),
   table.out = F,
   groups = NULL,
   remove.init = F
