af433d0366dbd11bd393a49d6f450f1026f3178a
jcaracappa1 <joseph.caracappa@noaa.gov>
2023-05-26 08:22:27 -0400

    mid sink option

---
+++ R/Satellite_Phytoplankton/plot_SatPhyto_BoxLayer.R
@@ -0,0 +1,73 @@
+#Plot forced phytoplankton groups in output and forcing files to compare
+library(dplyr)
+library(ggplot2)
+library(ncdf4)
+
+groups = c('Diatom','Dinoflagellates','Pico-phytoplankton')
+nc.var.name = c('Diatom_N','DinoFlag_N','PicoPhytopl_N')
+
+run.name = 'phyto_update_2022_newDiatomPct'
+# output.data = readRDS(here::here('Atlantis_Runs',run.name,'Post_Processed','Data','biomass_spatial_stanza.rds'))%>%
+#   filter(species %in% groups)
+output.nc = nc_open(here::here('Atlantis_Runs',run.name,'neus_output.nc'))
+output.dat.ls = list()
+for(i in 1:length(nc.var.name)){
+  
+  output.dat.ls[[i]] = ncvar_get(output.nc,nc.var.name[i]) %>%
+    reshape2::melt()%>%
+    rename(layer = 'Var1',box = 'Var2',time = 'Var3',biomass = 'value')%>%
+    mutate(group = nc.var.name[i],
+           time = time*73,
+           source = 'output')%>%
+    filter(biomass != 0)
+}
+output.dat = bind_rows(output.dat.ls)
+nc_close(output.nc)
+
+force.files = list.files(path = here::here('currentVersion','tsfiles','Annual_Files'),pattern = 'Phyto_',full.names = T)
+i=1
+force.dat.ls = list()
+for(i in 1:length(force.files)){
+  
+  force.year = nc_open(force.files)  
+  force.year.ls = list()
+  j=1
+  for(j in 1:length(nc.var.name)){
+    
+    force.year.ls[[j]] = x=  ncvar_get(force.year,nc.var.name[j]) %>%
+      reshape2::melt()%>%
+      rename(layer = 'Var1',box = 'Var2',doy = 'Var3',biomass = 'value')%>%
+      mutate(group = nc.var.name[j],
+             time = (doy-1+(365*(i-1))),
+             source = 'forcing')%>%
+      select(-doy)%>%
+      filter(!(is.na(biomass)))
+  }
+  force.dat.ls[[i]] = bind_rows(force.year.ls)
+}
+force.dat = bind_rows(force.dat.ls)
+
+# x = filter(force.dat,box ==1)
+# 
+# ggplot(x,aes(x=time,y=biomass,color = group))+geom_line()
+
+data.all = bind_rows(output.dat,force.dat)%>%
+  filter(!is.na(group))
+boxes = 0:29
+i=2
+
+pdf(here::here('Figures','SatPhyto_Comparisons.pdf'))
+for(i in 1:length(boxes)){
+  
+  data.box = data.all %>% filter(box == (boxes[i]+1))
+  if(nrow(data.box)==0){next()}
+  
+  p = ggplot(data.box, aes(x = time, y = biomass,color = source))+
+    geom_line()+
+    facet_grid(layer~group,scale = 'free_y')+
+    ggtitle(paste0('Box ',boxes[i]))
+   
+  gridExtra::grid.arrange(p)
+}
+dev.off()
+
--- R/call_run_comparisons.R
+++ R/call_run_comparisons.R
@@ -2,49 +2,39 @@
 source(here::here('R','plot_run_comparisons.R'))
 source(here::here('R','plot_run_catch_comparisons.R'))
 
-dev.old = '/home/jcaracappa/atlantis/Shared_Data/Dev_Runs/Dev_20230313/'
-dev = '/home/jcaracappa/atlantis/Shared_Data/Dev_Runs/Dev_20230327/'
-zl.1 = here::here('Atlantis_Runs','ZL_restore_1','')
-zl.2 = here::here('Atlantis_Runs','ZL_restore_2','')
-zl.2b = here::here('Atlantis_Runs','ZL_restore_2b','')
-zl.3 = here::here('Atlantis_Runs','ZL_restore_3','')
-zl.4 = here::here('Atlantis_Runs','ZL_restore_4','')
-zl.5 = here::here('Atlantis_Runs','ZL_restore_5','')
-zl.6 = here::here('Atlantis_Runs','ZL_restore_6','')
-zl.6b = here::here('Atlantis_Runs','ZL_restore_6b','')
-zl.7 = here::here('Atlantis_Runs','ZL_restore_7','')
-zl.8 = here::here('Atlantis_Runs','ZL_restore_8','')
-mak1 = here::here('Atlantis_Runs','MAK_fix_1','')
-mak2 = here::here('Atlantis_Runs','MAK_fix_2','')
-mak2.pred = here::here('Atlantis_Runs','MAK_fix_2_redDiet','')
-mak3 = here::here('Atlantis_Runs','MAK_fix_3','')
-mak23 = here::here('Atlantis_Runs','MAK_pers_23','')
-dev = here::here('Atlantis_Runs','dev_4_14_23','')
+# dev.old = '/home/jcaracappa/atlantis/Shared_Data/Dev_Runs/Dev_20230313/'
+# dev = '/net/work3/EDAB/atlantis/Rob_proj/Atlantis_Runs/dev_4_14_23/'
+dev = '/net/work3/EDAB/atlantis/Rob_proj/Atlantis_Runs/MAK_pers_37/'
+phyto.update = here::here('Atlantis_Runs','phyto_update_2022','')
+phyto.update.2 = here::here('Atlantis_Runs','phyto_update_2022_newDiatomPct','')
+phyto.update.3 = here::here('Atlantis_Runs','phyto_update_2022_lowVflux','')
+phyto.update.4 = here::here('Atlantis_Runs','phyto_update_2022_lowSink','')
+phyto.update.5 = here::here('Atlantis_Runs','phyto_update_2022_midSink','')
 
 figure.dir = here::here('Figures','Run_Comparisons','')
 
 plot_run_comparisons(
-  model.dirs = c(dev,mak23),
-  model.names = c('dev','MAK_pers_23'),
+  model.dirs = c(dev,phyto.update.4,phyto.update.5,phyto.update.2),
+  model.names = c('dev','lowSink_PL','midSink_PL','normalSink_PL'),
   plot.rel = T,
   plot.diff = F,
-  plot.out = paste(figure.dir,'MAK_pers_23_'),
-  table.out = F,
-  groups = c('MAK','HER','WHK','BLF','WPF','SUF','WIF','WTF','FOU','HAL','PLA','FLA','BFT','TUN','BIL','MPF','BUT','BPF','ANC','GOO','MEN','FDE','COD','SHK','OHK','NSH','OSH','LSQ','ISQ','ZL','ZM','ZS','ZG'),
-  remove.init = F
-)
-
-
-plot_run_comparisons(
-  model.dirs = c(dev.old,dev,zl.7),
-  model.names = c('Dev_20230313','Dev_20230327','ZL_restore_7'),
-  plot.rel = T,
-  plot.diff = F,
-  plot.out = paste(figure.dir,'ZL_restore_7_v_dev'),
+  plot.out = paste(figure.dir,'phyto_update_2022_sinkVel'),
   table.out = F,
   groups = NULL,
   remove.init = F
 )
+
+
+# plot_run_comparisons(
+#   model.dirs = c(dev.old,dev,zl.7),
+#   model.names = c('Dev_20230313','Dev_20230327','ZL_restore_7'),
+#   plot.rel = T,
+#   plot.diff = F,
+#   plot.out = paste(figure.dir,'ZL_restore_7_v_dev'),
+#   table.out = F,
+#   groups = NULL,
+#   remove.init = F
+# )
 
 # plot_run_catch_comparisons(
 #   model.dirs = c(dev, new.clams, new.clams.2),
