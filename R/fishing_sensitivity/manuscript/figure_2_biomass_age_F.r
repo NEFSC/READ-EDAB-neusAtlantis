5d44f7c446b92639d965a1b200d0ce3e9de7e04d
jcaracappa1 <joseph.caracappa@noaa.gov>
2023-09-22 11:54:13 -0500

    updated paths andd added margin axes

--- R/fishing_sensitivity/manuscript/figure_2_biomass_age_F.r
+++ R/fishing_sensitivity/manuscript/figure_2_biomass_age_F.r
@@ -36,19 +36,19 @@ source(paste0(here::here(), '/R/Post_Processing/process_atl_output.R'))
 source(paste0(here::here(), '/R/Post_Processing/make_atlantis_diagnostic_figures.R'))
 
 # Reading other files
-ssb_data <- readRDS(paste0(here::here(), '/ref_run_SSB_catch.RDS'))
-adult_age_data <- read.csv(paste0(here::here(), '/R/group_mature_age.csv'))
+ref.run.dir = '/net/work3/EDAB/atlantis/Shared_Data/fishing_sensitivity_manuscript/reference_run/fishing_sensitivity_baseline/'
+ssb_data <- readRDS(paste0(ref.run.dir, 'Post_Processed/Data/ref_run_SSB_catch.rds'))
+adult_age_data <- read.csv(here::here('diagnostics','group_mature_age.csv'))
 
-atl.dir = '/net/work3/EDAB/atlantis/Shared_Data/fishing_sensitivity_manuscript/reference_run/fishing_sensitivity_baseline/'
+param.dir = here::here('currentVersion')
+out.dir = paste0(ref.run.dir,'Post_Processed/Data/')
+fig.dir = '/net/work3/EDAB/atlantis/Shared_Data/fishing_sensitivity_manuscript/figures/manuscript/'
 
-param.dir = paste0(here::here(),'/currentVersion')
-out.dir = paste0(atl.dir,'Post_Processed/Data/')
-fig.dir = paste0(atl.dir,'Post_Processed/')
-
-reasonability_data <- read.csv(paste0(here::here(), '/output_diag_reasonability.csv'))
-priority_data <- read.csv(paste0(here::here(),'/neus_atlantis_group_priority.csv'))
+reasonability_data <- read.csv(here::here('data', 'output_diag_reasonability.csv'))
+priority_data <- read.csv(here::here('diagnostics','neus_atlantis_group_priority.csv'))%>%
+  rename(Overall = 'priority.overall',Code = 'code')
 priority_data <- arrange(priority_data,Overall,LongName)
-reference_points <-read.csv(paste0(here::here(), '/Atlantis_Ref_Points.csv'))
+reference_points <-read.csv(here::here('data','Atlantis_Ref_Points.csv'))
 groups_with_reference_points <- reference_points$Code
 
 #Run prefix is the filename prefix in the atlantis output (specified in run.bat)
@@ -56,11 +56,8 @@ run.prefix = 'neus_output'
 
 #Run function that retreives parameter files
 param.ls= get_atl_paramfiles(param.dir = param.dir,
-                             atl.dir=atl.dir,
+                             atl.dir=ref.run.dir,
                              include_catch=T)
-
-
-
 
 #Load groups data
 group.code = atlantistools::get_age_acronyms(param.ls$groups.file)
@@ -115,111 +112,133 @@ times_to_plot_migratory_3 <- c(0.6,1.6,2.6,3.6,4.6,5.6,6.6,7.6,8.6,9.6,10.6,11.6
 p <- 0
 hp <- 0
 
-graphics.off()
-png(paste0(here::here(), '/currentVersion/output/Post_Processed/Data/biomass_priority_3.png'),height=2000,width=2000, res= 216, pointsize = 4)
-par(mfrow=c(7,4))
-#for (g in 1:29) {
-#for (g in 30:59) {
-for (g in 60:87) {
-   groupName <- groupNames[g]
-   groupCode <- group.index$Code[group.index$LongName == groupName]
-    if ((groupCode != "DC") && (groupCode != "DL") && (groupCode != "DR") && (groupCode != "MA")) {
-      print(groupCode)
-      indices <- which(temp.plot.1$data$species == groupName)
-      ssb_group_data <- filter(ssb_data, species == groupName)
-      all_biomasses <- temp.plot.1$data$atoutput[indices]
-      all_times <- temp.plot.1$data$time[indices]
-      time_indices <- which(all_times %in% times_to_plot)
-      if (length(time_indices) == 1) {
-        time_indices <- which(all_times %in% times_to_plot_migratory)
-      }
-      if (length(time_indices) == 1) {
-        time_indices <- which(all_times %in% times_to_plot_migratory_2)
-      }
-      if (length(time_indices) == 1) {
-        time_indices <- which(all_times %in% times_to_plot_migratory_3)
-      }
-      time <- all_times[time_indices]
-      years <- time + 1964
-      time2 <- time
-      ssb_group_data <- filter(ssb_group_data, time %in% time2)
-      num_times<- length(time)
-      biomass <- all_biomasses[time_indices]
-      adult_biomass <- ssb_group_data$ssb
-      juvenile_biomass <- biomass - adult_biomass
-      juv_proportion <- juvenile_biomass / biomass
-      
-      group_reasonability <- filter(reasonability_data, LongName == groupName)
-      min_reasonability_q <- group_reasonability$Min_Target_q
-      max_reasonability_q <- group_reasonability$Max_Target_q
-      doReasonability <- !is.na(group_reasonability$Min_Target_q)
-      if(doReasonability) {
-        min_reasonability_q_line <- rep(min_reasonability_q, num_times)
-        max_reasonability_q_line <- rep(max_reasonability_q, num_times)
-      } else {
-        min_reasonability_q_line <- rep(0, num_times)
-        max_reasonability_q_line <- rep(0, num_times)
-      }
-      
-      min_reasonability_q <- group_reasonability$Min_Target / 100000
-      max_reasonability_q <- group_reasonability$Max_Target / 100000
+plot.index.df = data.frame(start = c(1,30,60),
+                           stop = c(29,59,87))
 
-      biomass <- biomass / 100000
-      
-      y_limit <- max(biomass)
-      if (doReasonability) {
-        if ((max_reasonability_q < (max(biomass) * 2)) && (max_reasonability_q > max(biomass))) {
-          y_limit <- max_reasonability_q
-        } 
-      }
-      
-      adult_proportion <- rep(1,length(juv_proportion))
-      par(mar=c(3,3.5,3,1.0))
-      
-      plot(years,biomass,type='l',main=groupName, col = col_vird_blue, lwd=1.5, bty = "n", ylim=c(0,y_limit), xaxt = "n", ylab="", xlab="", cex.axis=2.75, cex.main=2.75)
-      axis(1, at = c(1970, 1980, 1990, 2000, 2010, 2020), labels=F, cex.axis=5)
-    
-      
-      if (length(juv_proportion > 0)) {
-        par(new=TRUE)
-        plot(adult_proportion, type = "l", axes = FALSE, xlab ="", ylab = "", bty = "n", xaxt = "n", yaxt = "n", ylim = c(0,1), col = col_bb1)
-        polygon(c(1, seq(adult_proportion), length(adult_proportion)), c(0, adult_proportion, 0),
-                col = scales::alpha(col_bb1,0.5), border = NA)    
-        axis(4, lwd = 0, xaxt="n", yaxt = "n",ylim = c(0,1), labels=F, col=NA)
-      }
-      
-      if (length(juv_proportion > 0)) {
-        par(new=TRUE)
-        plot(juv_proportion, type = "l", lwd=0, axes = FALSE, xlab ="", ylab = "", vty = "n", xaxt = "n", yaxt = "n", ylim = c(0,1), col = col_bb2)
-        
-        polygon(c(1, seq(juv_proportion), length(juv_proportion)), c(0, juv_proportion, 0),
-                col = scales::alpha(col_bb2,0.5), border = NA)    
-        axis(4, lwd = 0, xaxt="n", yaxt = "n",ylim = c(0,1), labels=F, col=NA)
-      }
-      
-      if (nrow(ssb_group_data) > 0) {
-        par(new=TRUE)
-        
-        ssb_group_data <- mutate(ssb_group_data,F_rate = Catch/ssb)
-        F_rate <- ssb_group_data$F_rate
-        
-        plot(years,F_rate, type='l', col = col_vermillion, lty = 2, lwd=1.5, axes=FALSE,ylim=c(0,1),xaxt="n", yaxt="n",xlab="",ylab="")
-        axis(4, lwd = 0, xaxt="n", yaxt = "n",ylim = c(0,1), labels=F, col=NA)
-      }
-      
-#      if (doReasonability) {
-#        if (max_reasonability_q <= y_limit) {
-#          lines(years,max_reasonability_q_line, col=col_vermillion, lwd = 2, lty = 3, xlab="", ylab="", xaxt="n",yaxt="n")
-#        }
-#        if (min_reasonability_q <= y_limit) {
-#          lines(years,min_reasonability_q_line, col=col_vermillion, lwd = 2, lty = 3, xlab="", ylab="", xaxt="n",yaxt="n")
-#        }
-#        legend(15,y_limit*0.990, bty="n", legend=c("Biomass - surveyed (q corrected)","Biomass - surveyed"), col=c("blue","red"), lty=2:2, cex=0.8)
-#      }
+for(p.group in 1:nrow(plot.index.df)){
 
-    }    
+  graphics.off()
+  png(paste0(fig.dir, 'Figure_2_Biomass_F_',p.group,'.png'),height=2000,width=2000, res= 216, pointsize = 4)
+  par(mfrow=c(7,4),oma =c(5,10,2,5))
+  for (g in plot.index.df$start[p.group]:plot.index.df$stop[p.group]) {
+     groupName <- groupNames[g]
+     groupCode <- group.index$Code[group.index$LongName == groupName]
+      if ((groupCode != "DC") && (groupCode != "DL") && (groupCode != "DR") && (groupCode != "MA")) {
+        print(groupCode)
+        indices <- which(temp.plot.1$data$species == groupName)
+        ssb_group_data <- filter(ssb_data, species == groupName)
+        all_biomasses <- temp.plot.1$data$atoutput[indices]
+        all_times <- temp.plot.1$data$time[indices]
+        time_indices <- which(all_times %in% times_to_plot)
+        if (length(time_indices) == 1) {
+          time_indices <- which(all_times %in% times_to_plot_migratory)
+        }
+        if (length(time_indices) == 1) {
+          time_indices <- which(all_times %in% times_to_plot_migratory_2)
+        }
+        if (length(time_indices) == 1) {
+          time_indices <- which(all_times %in% times_to_plot_migratory_3)
+        }
+        time <- all_times[time_indices]
+        years <- time + 1964
+        time2 <- time
+        ssb_group_data <- filter(ssb_group_data, time %in% time2)
+        num_times<- length(time)
+        biomass <- all_biomasses[time_indices]
+        adult_biomass <- ssb_group_data$ssb
+        juvenile_biomass <- biomass - adult_biomass
+        juv_proportion <- juvenile_biomass / biomass
+        
+        group_reasonability <- filter(reasonability_data, LongName == groupName)
+        min_reasonability_q <- group_reasonability$Min_Target_q
+        max_reasonability_q <- group_reasonability$Max_Target_q
+        doReasonability <- !is.na(group_reasonability$Min_Target_q)
+        if(doReasonability) {
+          min_reasonability_q_line <- rep(min_reasonability_q, num_times)
+          max_reasonability_q_line <- rep(max_reasonability_q, num_times)
+        } else {
+          min_reasonability_q_line <- rep(0, num_times)
+          max_reasonability_q_line <- rep(0, num_times)
+        }
+        
+        min_reasonability_q <- group_reasonability$Min_Target / 100000
+        max_reasonability_q <- group_reasonability$Max_Target / 100000
+  
+        biomass <- biomass / 100000
+        
+        y_limit <- max(biomass)
+        if (doReasonability) {
+          if ((max_reasonability_q < (max(biomass) * 2)) && (max_reasonability_q > max(biomass))) {
+            y_limit <- max_reasonability_q
+          } 
+        }
+        
+        adult_proportion <- rep(1,length(juv_proportion))
+        par(mar=c(3,4,3,1.5))
+        
+        plot(years,biomass,type='l',main=groupName, col = col_vird_blue, lwd=1.5, bty = "n", ylim=c(0,y_limit), xaxt = "n", ylab="", xlab="", cex.axis=2.75, cex.main=2.75)
+        
+        plot.set = (plot.index.df$start[p.group]:plot.index.df$stop[p.group])
+        last.4 = tail(plot.set,4)
+        if(g %in% last.4){
+          axis(1, at = c(1970, 1980, 1990, 2000, 2010, 2020), labels=T, cex.axis=2)
+        }else{
+          axis(1, at = c(1970, 1980, 1990, 2000, 2010, 2020), labels=F, cex.axis=2)
+        }
+
+        right.7 = plot.set[(1:length(plot.set)) %% 4 == 0]
+        
+        if (length(juv_proportion > 0)) {
+          par(new=TRUE)
+          plot(adult_proportion, type = "l", axes = FALSE, xlab ="", ylab = "", bty = "n", xaxt = "n", yaxt = "n", ylim = c(0,1), col = col_bb1)
+          polygon(c(1, seq(adult_proportion), length(adult_proportion)), c(0, adult_proportion, 0),
+                  col = scales::alpha(col_bb1,0.5), border = NA)    
+          
+          if(g %in% right.7){
+            axis(4,at = c(0,0.5,1),labels = T,ylim = c(0,1),cex.axis = 2,las =2)
+          }else{
+            axis(4,at = c(0,0.5,1),labels = F,ylim = c(0,1),cex.axis = 2)
+          }
+        }
+        
+        if (length(juv_proportion > 0)) {
+          par(new=TRUE)
+          plot(juv_proportion, type = "l", lwd=0, axes = FALSE, xlab ="", ylab = "", vty = "n", xaxt = "n", yaxt = "n", ylim = c(0,1), col = col_bb2)
+          
+          polygon(c(1, seq(juv_proportion), length(juv_proportion)), c(0, juv_proportion, 0),
+                  col = scales::alpha(col_bb2,0.5), border = NA)    
+
+        }
+        
+        if (nrow(ssb_group_data) > 0) {
+          par(new=TRUE)
+          
+          ssb_group_data <- mutate(ssb_group_data,F_rate = Catch/ssb)
+          F_rate <- ssb_group_data$F_rate
+          
+          plot(years,F_rate, type='l', col = col_vermillion, lty = 2, lwd=1.5, axes=FALSE,ylim=c(0,1),xaxt="n", yaxt="n",xlab="",ylab="")
+          
+        }
+        
+
+        
+  #      if (doReasonability) {
+  #        if (max_reasonability_q <= y_limit) {
+  #          lines(years,max_reasonability_q_line, col=col_vermillion, lwd = 2, lty = 3, xlab="", ylab="", xaxt="n",yaxt="n")
+  #        }
+  #        if (min_reasonability_q <= y_limit) {
+  #          lines(years,min_reasonability_q_line, col=col_vermillion, lwd = 2, lty = 3, xlab="", ylab="", xaxt="n",yaxt="n")
+  #        }
+  #        legend(15,y_limit*0.990, bty="n", legend=c("Biomass - surveyed (q corrected)","Biomass - surveyed"), col=c("blue","red"), lty=2:2, cex=0.8)
+  #      }
+  
+      }    
+  }
+  
+  mtext('Biomass (000,000 mt)',2,3,outer = T,cex = 5)
+  # mtext('Year',1,2,outer = T, cex =3)
+  graphics.off()
 }
-graphics.off()
 
 
 png(paste0(here::here(), '/currentVersion/output/Post_Processed/Data/catch_priority.png'),width=1000,height=1000)
