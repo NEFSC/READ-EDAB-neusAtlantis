6cb1f0161ff077624294dbbd8ac256ad5fc2e2e7
andybeet <andrew.beet@noaa.gov>
2024-02-13 18:57:55 +0000

    sigularity required cd command to get into working directory

--- currentVersion/RunAtlantis.sh
+++ currentVersion/RunAtlantis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 find /app/model -type f | xargs dos2unix
-cd /app/model
+# required for singularity
+cd /app/model 
 atlantisMerged -i neus_init.nc 0 -o neus_output.nc -r at_run.prm -f at_force_LINUX.prm -p at_physics.prm -b at_biology.prm -h at_harvest.prm -e at_economics.prm -m neus_migrations_6681.csv -s neus_groups.csv -q neus_fisheries.csv -t . -d output
