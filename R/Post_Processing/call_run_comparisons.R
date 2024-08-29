8d1b3eef3204d3e87260152128233835b91b07c6
jcaracappa1 <joseph.caracappa@noaa.gov>
2024-08-29 15:20:44 -0500

    corrected spatial distributions in neus_init.nc and FXXX_SY

--- R/Post_Processing/call_run_comparisons.R
+++ R/Post_Processing/call_run_comparisons.R
@@ -2,21 +2,23 @@
 source(here::here('R','Post_Processing','plot_run_comparisons.R'))
 source(here::here('R','Post_Processing','plot_run_catch_comparisons.R'))
 
-fleet = '/net/work3/EDAB/atlantis/Andy_Proj/Atlantis_Runs/gfimposezerotsfull/'
-master = '/net/work3/EDAB/atlantis/Andy_Proj/Atlantis_Runs/master_2_2_0/'
+spatial1 = here::here('Atlantis_Runs','NE_groundfish_new_movement_2_WOL','/')
+spatial2 = here::here('Atlantis_Runs','NE_groundfish_new_movement_2','/')
+# master = '/net/work3/EDAB/atlantis/Andy_Proj/Atlantis_Runs/master_2_2_0/'
+dev = '/net/work3/EDAB/atlantis/Shared_Data/Dev_Runs/Dev_6681_20240620/'
 
 
 figure.dir = here::here('Figures','Run_Comparisons','')
 
-plot_run_catch_comparisons(model.dirs = c(master,fleet), model.names = c('master','fleet'),
-                           plot.out = paste0(figure.dir,'master_fleet_comparison'),
-                           plot.diff = F,plot.raw = T)
+# plot_run_catch_comparisons(model.dirs = c(master,fleet), model.names = c('master','fleet'),
+#                            plot.out = paste0(figure.dir,'master_fleet_comparison'),
+#                            plot.diff = F,plot.raw = T)
 plot_run_comparisons(
-  model.dirs = c(dev.6536,v6681.4m,v6681.5,v6681.6,v6681.6b,v6681.6c),
-  model.names = c('Dev_6536','v6681_Calib_4_merge','v6681_Calib_5','v6681_Calib_6','v6681_Calib_6b','v6681_Calib_6c_HER'),
+  model.dirs = c(dev,spatial1,spatial2),
+  model.names = c('Dev_6681','NE_groundfish_new_movement_2L','NE_groundfish_new_movement_2_WOL'),
   plot.rel = T,
   plot.diff = F,
-  plot.out = paste(figure.dir,'v6681_Calib_6c'),
+  plot.out = paste(figure.dir,'NE_groundfish_new_movement_2_WOL'),
   table.out = F,
   groups = NULL,
   remove.init = F
