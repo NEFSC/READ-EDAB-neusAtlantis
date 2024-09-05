4a06e10e3dcf1c1ea8ba34fe6feefd6aaae3c4a6
andybeet <22455149+andybeet@users.noreply.github.com>
2024-09-05 12:03:21 -0500

    changed the way species codes are joined to catch for plotting

--- R/Forcing_Fishing/load_catch_fishery.r
+++ R/Forcing_Fishing/load_catch_fishery.r
@@ -8,22 +8,28 @@
 load_catch_fishery <- function(dir, file_catch, fgs, verbose = FALSE) {
   
   file.catch <- file.path(dir, file_catch)
-  catchbio <- read.table(file.catch, header = TRUE)
-  fgs <- fgs[fgs$IsTurnedOn > 0, ]
-  colnames(fgs) <- tolower(colnames(fgs))
-  fishedlookup <- fgs[fgs$isfished > 0, ]
-  names(catchbio)[match(fishedlookup$code, names(catchbio))] <- fishedlookup$name
+  catchbio <- read.table(file.catch, header = TRUE) |>
+    dplyr::as_tibble()
+  fgsn <- fgs[fgs$IsTurnedOn > 0, ]
+  colnames(fgsn) <- tolower(colnames(fgsn))
+  fishedlookup <- fgsn[fgsn$isfished > 0, ]
+  #names(catchbio)[match(fishedlookup$code, names(catchbio))] <- fishedlookup$name
+  speciesCodes <- fishedlookup |>
+    dplyr::select(code,name) |>
+    dplyr::rename(species = name) |>
+    dplyr::as_tibble()
   
-  catchbio <- catchbio |> 
-    tidyr::pivot_longer(col=c(-Time,-Fishery)) 
-  
+  catchbiomass <- catchbio |> 
+    tidyr::pivot_longer(col=c(-Time,-Fishery)) |>
+    dplyr::left_join(speciesCodes, by = c("name"="code"))
+
   # out <- data.frame(species = catchbio$species, agecl = NA, 
   #                   polygon = NA, layer = NA, time = catchbio$Time, atoutput = catchbio$catchbio)
   # out <- out[order(out$species, out$time, out$polygon, out$agecl), ]
   
-  out <- catchbio |> 
+  out <- catchbiomass |> 
     dplyr::rename(time = Time,
-                  species = name,
+                  code = name,
                   atoutput = value,
                   fishery = Fishery) |>
     dplyr::arrange(species,time)
