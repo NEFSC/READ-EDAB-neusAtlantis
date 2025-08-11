8a4ef66458f635b1ec6f9664941192f33e45b0d3
andybeet <22455149+andybeet@users.noreply.github.com>
2025-08-11 18:21:33 -0400

    scallop swept area estimates using refined a, q values

--- data-raw/create_scallop_sweptarea.r
+++ data-raw/create_scallop_sweptarea.r
@@ -1,11 +1,11 @@
 #' Creates swept area biomass estimates by box for scallop
-#' 
+#'
 #' Uses the scallop survey and survdat sweptare to estmate by atlantis box
 #'
 #' Creates RDS files saved in data folder:
 #' sweptAreaBiomassScallop.RDS
 #'
-#' ALL BIOMASS VALUES ARE IN KG. 
+#' ALL BIOMASS VALUES ARE IN KG.
 
 library(magrittr)
 pullFromDB <- F
@@ -13,11 +13,12 @@ uid <- "" # change to your username
 server <- "" # select server
 
 # pull survey data
-# either pull raw data 
+# either pull raw data
 if (pullFromDB) {
-  channel <- dbutils::connect_to_database(server,uid)
+  channel <- dbutils::connect_to_database(server, uid)
   scallop <- survdat::get_survdat_scallop_data(channel)
-} else { # or read in previous pull
+} else {
+  # or read in previous pull
   # eventually this will reside on Github in version controlled package
   scallop <- readRDS(here::here("data-raw/scallop2025.rds"))
 }
@@ -29,49 +30,66 @@ data <- scallop$survdat
 
 #### Do similar thing but for each box in Atlantis
 # sweptarea Biomass by NEUS BOx
-neusBox <- sf::st_read(here::here("Geometry","gis"),layer="Neus_ll_0p01",quiet=T)
+neusBox <- sf::st_read(
+  here::here("Geometry", "gis"),
+  layer = "Neus_ll_0p01",
+  quiet = T
+)
 
 # select boxes. remove islands
-boxids <- neusBox |> 
-  dplyr::filter(BOX_ID != c("23","24") ) |> 
+boxids <- neusBox |>
+  dplyr::filter(BOX_ID != c("23", "24")) |>
   dplyr::pull(BOX_ID)
 
 #FALL
 biomassNEUSfall <- NULL
-for (boxid in boxids){ 
-  biomassBox <- survdat::calc_swept_area(surveyData=data,
-                                         areaPolygon = neusBox,
-                                         areaDescription="BOX_ID", 
-                                         filterByArea = boxid, 
-                                         filterBySeason = "FALL",
-                                         tidy=T)
+for (boxid in boxids) {
+  biomassBox <- survdat::calc_swept_area(
+    surveyData = data,
+    areaPolygon = neusBox,
+    areaDescription = "BOX_ID",
+    filterByArea = boxid,
+    filterBySeason = "FALL",
+    tidy = T,
+    q = 0.4,
+    a = .004515
+  )
   biomassBox$box <- boxid
-  biomassNEUSfall <- rbind(biomassNEUSfall,biomassBox)
+  biomassNEUSfall <- rbind(biomassNEUSfall, biomassBox)
 }
 
 #SPRING
 biomassNEUSspring <- NULL
-for (boxid in boxids){ 
-  biomassBox <- survdat::calc_swept_area(surveyData=data,
-                                         areaPolygon = neusBox,
-                                         areaDescription="BOX_ID", 
-                                         filterByArea = boxid, 
-                                         filterBySeason = "SPRING",
-                                         tidy=T)
+for (boxid in boxids) {
+  biomassBox <- survdat::calc_swept_area(
+    surveyData = data,
+    areaPolygon = neusBox,
+    areaDescription = "BOX_ID",
+    filterByArea = boxid,
+    filterBySeason = "SPRING",
+    tidy = T,
+    q = 0.4,
+    a = .004515
+  )
   biomassBox$box <- boxid
-  biomassNEUSspring <- rbind(biomassNEUSspring,biomassBox)
+  biomassNEUSspring <- rbind(biomassNEUSspring, biomassBox)
 }
 
-biomassNEUSspring <- biomassNEUSspring |> 
+biomassNEUSspring <- biomassNEUSspring |>
   dplyr::mutate(season = "SPRING")
-biomassNEUSfall <- biomassNEUSfall |> 
+biomassNEUSfall <- biomassNEUSfall |>
   dplyr::mutate(season = "FALL")
 
-biomassNEUS <- rbind(biomassNEUSspring,biomassNEUSfall)
+biomassNEUS <- rbind(biomassNEUSspring, biomassNEUSfall)
 
-sweptAreaBiomassBox <- biomassNEUS %>% 
-  dplyr::filter(variable %in% c("tot.biomass","tot.bio.var","tot.abundance","tot.abundance.var")) %>%
+sweptAreaBiomassBox <- biomassNEUS %>%
+  dplyr::filter(
+    variable %in%
+      c("tot.biomass", "tot.bio.var", "tot.abundance", "tot.abundance.var")
+  ) %>%
   tibble::as_tibble()
 
-saveRDS(sweptAreaBiomassBox,file = here::here("data","sweptAreaBiomassScallop.RDS"))
-
+saveRDS(
+  sweptAreaBiomassBox,
+  file = here::here("data", "sweptAreaBiomassScallop.RDS")
+)
