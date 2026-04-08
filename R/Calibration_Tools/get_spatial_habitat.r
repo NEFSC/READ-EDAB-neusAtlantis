2a859a66f4c49792a9c7aa0d62738501ec21ff38
Robert Gamble <Robert.Gamble@noaa.gov>
2026-04-08 12:30:20 -0400

    Update get_spatial_habitat.r
    Includes outputs to screen.

--- R/Calibration_Tools/get_spatial_habitat.r
+++ R/Calibration_Tools/get_spatial_habitat.r
@@ -7,6 +7,7 @@ get_minmax_thresh <- function(sppname) {
 #  biofile <- here::here('currentVersion', '/at_biology.prm')
   threshold_file <- here::here('data', '/NRHA_Thresholds.csv')
   threshold_df <- read.table(threshold.dir, sep = ",", header = TRUE, stringsAsFactors = FALSE)
+  threshold_df <- read.table(threshold_file, sep = ",", header = TRUE, stringsAsFactors = FALSE)
 
   spp_threshold_info <- filter(threshold_df,Code == sppname)
   spp_min_temp <- spp_threshold_info$min_bottom_temp
@@ -16,8 +17,8 @@ get_minmax_thresh <- function(sppname) {
 }
 
 
-sppname <- c("COD")
-sppnames <- c("COD")
+sppname <- c("HAL")
+sppnames <- c("COD","HAL")
 reference.dir <- here::here('Atlantis_Runs', 'dev_7_22_25','/neus_outputBoxBiomass.txt')
 reference_box_df <- read.table(reference.dir, sep = "", header = TRUE, stringsAsFactors = FALSE)
 ref_df <- filter(reference_box_df, Box > 0 & Box <= 22)
@@ -60,96 +61,36 @@ spp_minmaxtemp_df <- inner_join(minmaxtemp_df,summary_ref_df, by = c('Box'))
 overallMin <- 100
 overallMax <- -100
 for (p in 1:22) {
-  minmaxtemp_df[p,1] <- p
+  spp_minmaxtemp_df[p,1] <- p
   polygon_df <- filter(temp_df,polygon == p)
-  minmaxtemp_df$Min[p] <- min(polygon_df$atoutput)
-  if (minmaxtemp_df$Min[p] < overallMin) {
-    overallMin <- minmaxtemp_df$Min[p]
+  spp_minmaxtemp_df$Min[p] <- min(polygon_df$atoutput)
+  if (spp_minmaxtemp_df$Min[p] < overallMin) {
+    overallMin <- spp_minmaxtemp_df$Min[p]
   }
-  minmaxtemp_df$Max[p] <- max(polygon_df$atoutput)
-  if (minmaxtemp_df$Max[p] > overallMax) {
-    overallMax <- minmaxtemp_df$Max[p]
+  spp_minmaxtemp_df$Max[p] <- max(polygon_df$atoutput)
+  if (spp_minmaxtemp_df$Max[p] > overallMax) {
+    overallMax <- spp_minmaxtemp_df$Max[p]
   }
 }
 
 minmax_vector <- get_minmax_thresh(sppname)
-final_output <- df %>%
-
-  dplyr::mutate(period = "baseline")
-
-############### END END END END ##########################
-
-test_df <- get_habitable_boxes(
-  param.ls,
-  speciesCodes = sppname,
-  timeFrame = -1
-)
-
-test_layers_df <- rename(test_df, Box = habitableBoxes)
-test_layers_df <- inner_join(test_layers_df, summary_ref_df, by="Box")
-# Remove boxes from test_df that aren't in the reference spatial extent
-
-test_boxes_df <- distinct(select(test_layers_df, c(group,Box)))
-test_boxes_df <- arrange(test_boxes_df,Box)
-
-expected_layers <- nrow(test_layers_df)
-expected_boxes <- nrow(test_boxes_df)
-
-
-param.ls <- atlantisdiagnostics::get_atl_paramfiles(
-  param.dir,
-  run.dir,
-  run.prefix,
-  include_catch = F
-)
-
-habitable_df <- get_habitable_boxes(
-  param.ls,
-  speciesCodes = sppnames,
-  timeFrame = -1
-)
-habitable_df <- distinct(select(habitable_df, c(group,habitableBoxes)))
-habitable_df <- arrange(habitable_df,habitableBoxes)
-
-
-
-reference_box_df <- read.table(reference.dir, sep = "", header = TRUE, stringsAsFactors = FALSE)
-compare_df <- read.table(reference.dir, sep = "", header = TRUE, stringsAsFactors = FALSE)
-
-ref_df <- filter(reference_box_df, Box > 0 & Box <= 22)
-comp_df <- filter(compare_df, Box > 0 & Box <= 22)
-
-ref_df <- filter(ref_df, Time >= 16790)
-summary_ref_df <- group_by(ref_df, Box)
-summary_ref_df <- summarise(summary_ref_df,across(everything(), sum))
-
-summary_total_df <- summarise(ref_df,across(everything(), sum))
-
-num_ref_rows <- nrow(summary_ref_df)
-
-ref_pct_df <- summary_ref_df
-
-for (r in 1:num_ref_rows) {
-
-  ref_pct_df[r,3:88] <- summary_ref_df[r,3:88] / summary_total_df[1,3:88]
-
+spp_min <- minmax_vector[1]
+spp_max <- minmax_vector[2]
+final_output <- mutate(spp_minmaxtemp_df, habitable = "0")
+nboxes <- nrow(final_output)
+for (i in 1: nboxes) {
+  if (spp_minmaxtemp_df$Min[i] < spp_min) {
+    final_output$habitable[i] <- -1
+  }
+  if (spp_minmaxtemp_df$Max[i] > spp_max) {
+    final_output$habitable[i] <- 1
+  }
+  if ((spp_minmaxtemp_df$Min[i] < spp_min) && (spp_minmaxtemp_df$Max[i] > spp_max)) {
+    final_output$habitable[i] <- 99
+  }
 }
 
-comp_df <- filter(ref_df, Time >= 20805)
-summary_comp_df <- group_by(comp_df, Box)
-summary_comp_df <- summarise(summary_comp_df,across(everything(), sum))
-
-summary_total_comp_df <- summarise(comp_df,across(everything(), sum))
-
-num_comp_rows <- nrow(summary_comp_df)
-
-comp_pct_df <- summary_comp_df
-
-for (r in 1:num_comp_rows) {
-
-  comp_pct_df[r,3:88] <- summary_comp_df[r,3:88] / summary_total_comp_df[1,3:88]
-
-}
-
-reference_pa_df <- ref_df / ref_df
-reference_pa_df[is.na(reference_pa_df)] <- 0
+unsuitable_boxes <- filter(final_output, habitable !=0)
+print(paste0('Species = ', sppname, " : Min = ", spp_min, " : Max = ", spp_max))
+print(unsuitable_boxes)
+print(final_output)
